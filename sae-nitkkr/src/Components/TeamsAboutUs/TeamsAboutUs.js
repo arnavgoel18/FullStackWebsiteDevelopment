98aee02afca5b30ae049226fe62eaccf729bc0d3
mittalkeshav999 <89253638+mittalkeshav999@users.noreply.github.com>
2021-09-28 11:49:03 +0530

    Team about us uploaded

---
+++ sae-nitkkr/src/Components/TeamsAboutUs/TeamsAboutUs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Acc() {
+  return (
+    <div>
+       <div className="heading"><h2 className="weare"> we </h2><h2 className="weare"> are</h2><h2 className="boldcolor">ACCELERONS</h2></div>
+    <div className="text_container"><div className="text">TEAM ACCELERONS is a student-led engineering team of SAE NIT Kurukshetra. The feeling of speed and precision, the tactical aspects, the way car looks, the way it behaves, that first lap excitement and the intrigues that surround it, are the things that drives the team to DESIGN, BUILD and RACE formula-style cars for the Formula Bharat and SUPRA competitions.
+
+From being a first-timer in SUPRA 2014 to standing on top among all NITS in SUPRA 2018, the Team has come a long way, upgrading itself with each passing season.. After all Felipe Massa rightly said, "When you give up your hunger for success you are not racing full heartedly anymore.</div></div>
+    <div className="bigcontainer">
+      <div className="lowertext">
+        <div className="text">Till date, ACCELRONS's F4 has shown its grip in the
+          following competitions:</div>
+        <div className="comp">
+          SUPRA <br />
+          FORMULA BHARAT
+        </div>
+      </div>
+      <div className="image_container"><div className="image"><img src="./images/Accelrons.jpg" alt="image" /></div></div>
+    </div>
+    </div>
+  )
+}
+
+export default Acc
