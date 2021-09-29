1238861c577d5dc6bafaf31dba335fb44fde8717
mittalkeshav999 <89253638+mittalkeshav999@users.noreply.github.com>
2021-09-30 01:32:11 +0530

    BrandAmbassador code uploaded

---
+++ sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/BrandAmbassador.js
@@ -0,0 +1,37 @@
+import "./BrandAmbassador.css";
+
+function BrandAmbassador() {
+  return (
+    <>
+      <div className="boxes">
+        <div className="box1">
+          <div className="box11">
+            <div className="box111">AUTOKRITI</div>{" "}
+            <div className="box112"> x</div>{" "}
+          </div>
+          <div className="box12">
+            <div className="box121">BRAND</div>{" "}
+            <div className="box122">AMBASSADOR </div>{" "}
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="box2">
+          <div className="box21">Become</div>
+          <div className="box22">our</div>
+          <dic className="box23">BRAND AMBASSADOR</dic>
+        </div>
+        <br />
+        <div className="box3">
+          Spread a word about us in your college and get amazing perks!
+        </div>
+        <br />
+        <br />
+        <br />
+        <button className="box4">JOIN US</button>
+      </div>
+    </>
+  );
+}
+
+export default BrandAmbassador;
