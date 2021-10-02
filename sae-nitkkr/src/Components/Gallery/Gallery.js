c21ad38638fdef1a33a2aa0232471271ee1ff0f2
sourabh <buckalsourabh@gmail.com>
2021-10-02 16:47:33 +0530

    gallery code updated

---
+++ sae-nitkkr/src/Components/Gallery/Gallery.js
@@ -0,0 +1,74 @@
+import './Gallery.css';
+import one1 from '../../Assets/one1.jpg';
+import one from '../../Assets/one.JPG';
+import one2 from '../../Assets/one2.jpg';
+
+function App() {
+  return (
+    <div>
+      <h1>PHOTOS</h1>
+      <Gallery />
+    </div>
+  );
+}
+
+
+function Gallery() {
+  return (
+    <>
+
+      <div class="row">
+        <div class="column">
+          <img src={one1} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one1} />
+        </div>
+        <div class="column">
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+        </div>
+        <div class="column">
+          <img src={one1} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one1} />
+        </div>
+        <div class="column">
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+        </div>
+        <div class="column">
+          <img src={one1} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one} />
+          <img src={one2} />
+          <img src={one} />
+          <img src={one1} />
+        </div>
+
+      </div>
+
+
+
+
+    </>
+
+  );
+}
+export default App;
--- sae-nitkkr/src/index.js
+++ sae-nitkkr/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Gallery from './Components/Gallery';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    
+  </>,
   document.getElementById('root')
 );
 
