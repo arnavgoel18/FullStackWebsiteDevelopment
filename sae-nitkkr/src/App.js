730039271cf9fd041c291fd589a2dc11fc8c033e
arnavgoel18 <arnav18.goel@gmail.com>
2021-10-14 16:24:28 +0530

    Changed App.js
    Added rect-router-dom

--- sae-nitkkr/src/App.js
+++ sae-nitkkr/src/App.js
@@ -1,17 +1,22 @@
 // import FooterWhite from "./Components/Footer/Footer(white)/FooterWhite";
-import FirstDisplayDiv from './Components/FirstDisplayDIV/FirstDisplayDIV';
-import ProfIncharges from './Containers/HomePage/ProfInchargesAndManagers/ProfIncharges'
-import Managers from './Containers/HomePage/ProfInchargesAndManagers/Managers'
+// import FirstDisplayDiv from './Components/FirstDisplayDIV/FirstDisplayDIV';
+// import ProfIncharges from './Containers/HomePage/ProfInchargesAndManagers/ProfIncharges'
+// import Managers from './Containers/HomePage/ProfInchargesAndManagers/Managers'
+// import AboutUs from './Components/AboutUs/AboutUs'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutUs from './Components/AboutUs/AboutUs'
+import Footer from './Components/Footer/Footer(black)/FooterBlack'
 
 
 function App() {
   return (
     <>
-      <FirstDisplayDiv page = "Accelerons"/>
-      <AboutUs/>
-      <ProfIncharges/>
-      <Managers/>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={() => <Footer />} />
+          <Route path="/about" exact component={() => <AboutUs />} />
+        </Switch>
+      </Router>
     </>
   );
 }
