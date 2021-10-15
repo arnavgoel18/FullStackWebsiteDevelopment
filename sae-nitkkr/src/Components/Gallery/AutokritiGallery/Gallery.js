524b73d6e4b68509146127c3c3f12853080adc5a
Navneet-Raj99 <89241731+Navneet-Raj99@users.noreply.github.com>
2021-10-16 00:25:05 +0530

    completed with gallery work of nitrox and autokriti

--- sae-nitkkr/src/Components/Gallery/AutokritiGallery/Gallery.js
+++ sae-nitkkr/src/Components/Gallery/AutokritiGallery/Gallery.js
@@ -3,9 +3,38 @@ import one1 from "../../../Assets/one1.jpg";
 import one from '../../../Assets/one.JPG';
 import one2 from '../../../Assets/one2.jpg';
 
-import Autokriti1 from '../../../Assets/AutokritiGallery/Autokritigallery_1.jpg'
+import Autokriti1 from '../../../Assets/AutokritiGallery/Autokritigallery_1.jpeg'
 import Autokriti2 from '../../../Assets/AutokritiGallery/Autokritigallery_2.jpg'
 import Autokriti3 from '../../../Assets/AutokritiGallery/Autokritigallery_3.jpg'
+import Autokriti4 from '../../../Assets/AutokritiGallery/Autokritigallery_4.jpg'
+import Autokriti5 from '../../../Assets/AutokritiGallery/Autokritigallery_5.jpg'
+import Autokriti6 from '../../../Assets/AutokritiGallery/Autokritigallery_6.jpg'
+import Autokriti7 from '../../../Assets/AutokritiGallery/Autokritigallery_7.jpg'
+import Autokriti8 from '../../../Assets/AutokritiGallery/Autokritigallery_8.jpg'
+import Autokriti9 from '../../../Assets/AutokritiGallery/Autokritigallery_9.jpg'
+import Autokriti10 from '../../../Assets/AutokritiGallery/Autokritigallery_10.jpg'
+import Autokriti11 from '../../../Assets/AutokritiGallery/Autokritigallery_11.jpg'
+import Autokriti12 from '../../../Assets/AutokritiGallery/Autokritigallery_12.jpg'
+import Autokriti13 from '../../../Assets/AutokritiGallery/Autokritigallery_13.jpg'
+import Autokriti14 from '../../../Assets/AutokritiGallery/Autokritigallery_14.jpg'
+import Autokriti15 from '../../../Assets/AutokritiGallery/Autokritigallery_15.jpg'
+import Autokriti16 from '../../../Assets/AutokritiGallery/Autokritigallery_16.jpg'
+import Autokriti17 from '../../../Assets/AutokritiGallery/Autokritigallery_17.jpg'
+import Autokriti18 from '../../../Assets/AutokritiGallery/Autokritigallery_18.jpg'
+import Autokriti19 from '../../../Assets/AutokritiGallery/Autokritigallery_19.jpg'
+import Autokriti20 from '../../../Assets/AutokritiGallery/Autokritigallery_20.jpg'
+import Autokriti21 from '../../../Assets/AutokritiGallery/Autokritigallery_21.jpg'
+import Autokriti22 from '../../../Assets/AutokritiGallery/Autokritigallery_22.jpg'
+import Autokriti23 from '../../../Assets/AutokritiGallery/Autokritigallery_23.jpg'
+import Autokriti24 from '../../../Assets/AutokritiGallery/Autokritigallery_24.jpg'
+import Autokriti25 from '../../../Assets/AutokritiGallery/Autokritigallery_25.jpg'
+import Autokriti26 from '../../../Assets/AutokritiGallery/Autokritigallery_26.JPG'
+import Autokriti27 from '../../../Assets/AutokritiGallery/Autokritigallery_27.JPG'
+import Autokriti28 from '../../../Assets/AutokritiGallery/Autokritigallery_28.JPG'
+import Autokriti29 from '../../../Assets/AutokritiGallery/Autokritigallery_29.JPG'
+import Autokriti30 from '../../../Assets/AutokritiGallery/Autokritigallery_30.jpg'
+
+
 
 
 function App() {
@@ -26,44 +55,44 @@ function Gallery() {
           <img src={Autokriti1} alt="hello" className="image_of_gallery" />
           <img src={Autokriti2} alt="hello" className="image_of_gallery" />
           <img src={Autokriti3} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one1} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti4} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti5} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti6} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti7} alt="hello" className="image_of_gallery" />
         </div>
         <div className="Gallery-column">
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti8} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti9} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti10} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti11} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti12} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti13} alt="hello" className="image_of_gallery" />
         </div>
         <div className="Gallery-column">
-          <img src={one1} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one1} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti14} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti15} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti16} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti17} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti18} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti19} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti20} alt="hello" className="image_of_gallery" />
         </div>
         <div className="Gallery-column">
-          <img src={one2} className="image_of_gallery"  />
-          <img src={one} className="image_of_gallery" />
-          <img src={one} className="image_of_gallery" />
-          <img src={one2} className="image_of_gallery"  />
-          <img src={one} className="image_of_gallery" />
-          <img src={one} className="image_of_gallery"  />
+          <img src={Autokriti21} className="image_of_gallery"  />
+          <img src={Autokriti22} className="image_of_gallery" />
+          <img src={Autokriti23} className="image_of_gallery" />
+          <img src={Autokriti24} className="image_of_gallery"  />
+          <img src={Autokriti25} className="image_of_gallery" />
+          <img src={Autokriti26} className="image_of_gallery"  />
         </div>
         <div className="Gallery-column">
-          <img src={one1} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one1} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti27} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti28} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti29} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti30} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti3} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti13} alt="hello" className="image_of_gallery" />
+          <img src={Autokriti23} alt="hello" className="image_of_gallery" />
         </div>
       </div>
       <br/><br/>
--- sae-nitkkr/src/Components/Gallery/NitroxGallery/Gallery.js
+++ sae-nitkkr/src/Components/Gallery/NitroxGallery/Gallery.js
@@ -3,6 +3,43 @@ import one1 from "../../../Assets/one1.jpg";
 import one from '../../../Assets/one.JPG';
 import one2 from '../../../Assets/one2.jpg';
 
+import Nitrox1 from '../../../Assets/NitroxGallery/Nitroxgallery_1.jpg'
+import Nitrox2 from '../../../Assets/NitroxGallery/Nitroxgallery_2.JPG'
+// import Nitrox3 from '../../../Assets/NitroxGallery/Nitroxgallery_3.jpg'
+// import Nitrox4 from '../../../Assets/NitroxGallery/Nitroxgallery_4.jpg'
+import Nitrox5 from '../../../Assets/NitroxGallery/Nitroxgallery_5.JPG'
+import Nitrox6 from '../../../Assets/NitroxGallery/Nitroxgallery_6.JPG'
+// import Nitrox7 from '../../../Assets/NitroxGallery/Nitroxgallery_7.jpg'
+import Nitrox8 from '../../../Assets/NitroxGallery/Nitroxgallery_8.JPG'
+import Nitrox9 from '../../../Assets/NitroxGallery/Nitroxgallery_9.JPG'
+import Nitrox10 from '../../../Assets/NitroxGallery/Nitroxgallery_10.jpg'
+import Nitrox11 from '../../../Assets/NitroxGallery/Nitroxgallery_11.jpg'
+import Nitrox12 from '../../../Assets/NitroxGallery/Nitroxgallery_12.jpg'
+import Nitrox13 from '../../../Assets/NitroxGallery/Nitroxgallery_13.jpg'
+// import Nitrox14 from '../../../Assets/NitroxGallery/Nitroxgallery_14.jpg'
+import Nitrox15 from '../../../Assets/NitroxGallery/Nitroxgallery_15.jpg'
+import Nitrox16 from '../../../Assets/NitroxGallery/Nitroxgallery_16.jpg'
+import Nitrox17 from '../../../Assets/NitroxGallery/Nitroxgallery_17.jpg'
+import Nitrox18 from '../../../Assets/NitroxGallery/Nitroxgallery_18.jpg'
+// import Nitrox19 from '../../../Assets/NitroxGallery/Nitroxgallery_19.jpg'
+import Nitrox20 from '../../../Assets/NitroxGallery/Nitroxgallery_20.jpg'
+import Nitrox21 from '../../../Assets/NitroxGallery/Nitroxgallery_21.jpg'
+// import Nitrox22 from '../../../Assets/NitroxGallery/Nitroxgallery_22.jpg'
+import Nitrox23 from '../../../Assets/NitroxGallery/Nitroxgallery_23.jpg'
+// import Nitrox24 from '../../../Assets/NitroxGallery/Nitroxgallery_24.jpg'
+import Nitrox25 from '../../../Assets/NitroxGallery/Nitroxgallery_25.jpg'
+import Nitrox26 from '../../../Assets/NitroxGallery/Nitroxgallery_26.JPG'
+import Nitrox36 from '../../../Assets/NitroxGallery/Nitroxgallery_36.jpg'
+import Nitrox61 from '../../../Assets/NitroxGallery/Nitroxgallery_61.JPG'
+import Nitrox62 from '../../../Assets/NitroxGallery/Nitroxgallery_62.JPG'
+import Nitrox65 from '../../../Assets/NitroxGallery/Nitroxgallery_65.JPG'
+import Nitrox66 from '../../../Assets/NitroxGallery/Nitroxgallery_66.JPG'
+import Nitrox71 from '../../../Assets/NitroxGallery/Nitroxgallery_71.JPG'
+import Nitrox75 from '../../../Assets/NitroxGallery/Nitroxgallery_75.JPG'
+import Nitrox78 from '../../../Assets/NitroxGallery/Nitroxgallery_78.JPG'
+
+
+
 function App() {
   return (
     <div className="Gallery-Main_Cotainer">
@@ -18,47 +55,47 @@ function Gallery() {
     <>
       <div className="Gallery-row">
         <div className="Gallery-column">
-          <img src={one1} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one1} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox1} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox2} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox5} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox6} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox8} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox9} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox10} alt="hello" className="image_of_gallery" />
         </div>
         <div className="Gallery-column">
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox11} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox12} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox13} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox15} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox16} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox17} alt="hello" className="image_of_gallery" />
         </div>
         <div className="Gallery-column">
-          <img src={one1} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one1} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox18} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox20} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox21} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox23} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox2} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox25} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox26} alt="hello" className="image_of_gallery" />
         </div>
         <div className="Gallery-column">
-          <img src={one2} className="image_of_gallery"  />
-          <img src={one} className="image_of_gallery" />
-          <img src={one} className="image_of_gallery" />
-          <img src={one2} className="image_of_gallery"  />
-          <img src={one} className="image_of_gallery" />
-          <img src={one} className="image_of_gallery"  />
+          <img src={Nitrox36} className="image_of_gallery"  />
+          <img src={Nitrox61} className="image_of_gallery" />
+          <img src={Nitrox62} className="image_of_gallery" />
+          <img src={Nitrox65} className="image_of_gallery"  />
+          <img src={Nitrox66} className="image_of_gallery" />
+          <img src={Nitrox10} className="image_of_gallery"  />
         </div>
         <div className="Gallery-column">
-          <img src={one1} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one2} alt="hello" className="image_of_gallery" />
-          <img src={one} alt="hello" className="image_of_gallery" />
-          <img src={one1} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox10} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox20} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox5} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox9} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox75} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox71} alt="hello" className="image_of_gallery" />
+          <img src={Nitrox78} alt="hello" className="image_of_gallery" />
         </div>
       </div>
       <br/><br/>
