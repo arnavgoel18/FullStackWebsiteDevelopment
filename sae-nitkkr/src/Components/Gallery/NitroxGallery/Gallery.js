56aa3ece1d7040dd4bdaeead3a3514a053e4024f
sourabh <buckalsourabh@gmail.com>
2021-11-02 11:08:19 +0530

    gallery updated

--- sae-nitkkr/src/Components/Gallery/NitroxGallery/Gallery.js
+++ sae-nitkkr/src/Components/Gallery/NitroxGallery/Gallery.js
@@ -1,96 +1,189 @@
-import './Gallery.css';
+import react, { useState } from "react";
+import "./Gallery.css";
 
+import Nitrox1 from "../../../Assets/NitroxGallery/Nitroxgallery_1.jpg";
+import Nitrox2 from "../../../Assets/NitroxGallery/Nitroxgallery_2.JPG";
+import Nitrox5 from "../../../Assets/NitroxGallery/Nitroxgallery_5.JPG";
+import Nitrox6 from "../../../Assets/NitroxGallery/Nitroxgallery_6.JPG";
+import Nitrox8 from "../../../Assets/NitroxGallery/Nitroxgallery_8.JPG";
+import Nitrox9 from "../../../Assets/NitroxGallery/Nitroxgallery_9.JPG";
+import Nitrox10 from "../../../Assets/NitroxGallery/Nitroxgallery_10.jpg";
+import Nitrox11 from "../../../Assets/NitroxGallery/Nitroxgallery_11.jpg";
+import Nitrox12 from "../../../Assets/NitroxGallery/Nitroxgallery_12.jpg";
+import Nitrox13 from "../../../Assets/NitroxGallery/Nitroxgallery_13.jpg";
+import Nitrox15 from "../../../Assets/NitroxGallery/Nitroxgallery_15.jpg";
+import Nitrox16 from "../../../Assets/NitroxGallery/Nitroxgallery_16.jpg";
+import Nitrox17 from "../../../Assets/NitroxGallery/Nitroxgallery_17.jpg";
+import Nitrox18 from "../../../Assets/NitroxGallery/Nitroxgallery_18.jpg";
+import Nitrox20 from "../../../Assets/NitroxGallery/Nitroxgallery_20.jpg";
+import Nitrox21 from "../../../Assets/NitroxGallery/Nitroxgallery_21.jpg";
+import Nitrox23 from "../../../Assets/NitroxGallery/Nitroxgallery_23.jpg";
+import Nitrox25 from "../../../Assets/NitroxGallery/Nitroxgallery_25.jpg";
+import Nitrox26 from "../../../Assets/NitroxGallery/Nitroxgallery_26.jpg";
+import Nitrox36 from "../../../Assets/NitroxGallery/Nitroxgallery_36.jpg";
+import Nitrox61 from "../../../Assets/NitroxGallery/Nitroxgallery_61.JPG";
+import Nitrox62 from "../../../Assets/NitroxGallery/Nitroxgallery_62.JPG";
+import Nitrox65 from "../../../Assets/NitroxGallery/Nitroxgallery_65.JPG";
+import Nitrox66 from "../../../Assets/NitroxGallery/Nitroxgallery_66.JPG";
+import Nitrox71 from "../../../Assets/NitroxGallery/Nitroxgallery_71.JPG";
+import Nitrox75 from "../../../Assets/NitroxGallery/Nitroxgallery_75.JPG";
+import Nitrox78 from "../../../Assets/NitroxGallery/Nitroxgallery_78.JPG";
 
-import Nitrox1 from '../../../Assets/NitroxGallery/Nitroxgallery_1.jpg'
-import Nitrox2 from '../../../Assets/NitroxGallery/Nitroxgallery_2.JPG'
-import Nitrox5 from '../../../Assets/NitroxGallery/Nitroxgallery_5.JPG'
-import Nitrox6 from '../../../Assets/NitroxGallery/Nitroxgallery_6.JPG'
-import Nitrox8 from '../../../Assets/NitroxGallery/Nitroxgallery_8.JPG'
-import Nitrox9 from '../../../Assets/NitroxGallery/Nitroxgallery_9.JPG'
-import Nitrox10 from '../../../Assets/NitroxGallery/Nitroxgallery_10.jpg'
-import Nitrox11 from '../../../Assets/NitroxGallery/Nitroxgallery_11.jpg'
-import Nitrox12 from '../../../Assets/NitroxGallery/Nitroxgallery_12.jpg'
-import Nitrox13 from '../../../Assets/NitroxGallery/Nitroxgallery_13.jpg'
-import Nitrox15 from '../../../Assets/NitroxGallery/Nitroxgallery_15.jpg'
-import Nitrox16 from '../../../Assets/NitroxGallery/Nitroxgallery_16.jpg'
-import Nitrox17 from '../../../Assets/NitroxGallery/Nitroxgallery_17.jpg'
-import Nitrox18 from '../../../Assets/NitroxGallery/Nitroxgallery_18.jpg'
-import Nitrox20 from '../../../Assets/NitroxGallery/Nitroxgallery_20.jpg'
-import Nitrox21 from '../../../Assets/NitroxGallery/Nitroxgallery_21.jpg'
-import Nitrox23 from '../../../Assets/NitroxGallery/Nitroxgallery_23.jpg'
-import Nitrox25 from '../../../Assets/NitroxGallery/Nitroxgallery_25.jpg'
-import Nitrox26 from '../../../Assets/NitroxGallery/Nitroxgallery_26.jpg'
-import Nitrox36 from '../../../Assets/NitroxGallery/Nitroxgallery_36.jpg'
-import Nitrox61 from '../../../Assets/NitroxGallery/Nitroxgallery_61.JPG'
-import Nitrox62 from '../../../Assets/NitroxGallery/Nitroxgallery_62.JPG'
-import Nitrox65 from '../../../Assets/NitroxGallery/Nitroxgallery_65.JPG'
-import Nitrox66 from '../../../Assets/NitroxGallery/Nitroxgallery_66.JPG'
-import Nitrox71 from '../../../Assets/NitroxGallery/Nitroxgallery_71.JPG'
-import Nitrox75 from '../../../Assets/NitroxGallery/Nitroxgallery_75.JPG'
-import Nitrox78 from '../../../Assets/NitroxGallery/Nitroxgallery_78.JPG'
-
-
-
-function App() {
-  return (
-    <div className="Gallery-Main_Cotainer">
-      <br/><br/>
-      {/* <h1 className="Gallery-photos">PHOTOS</h1> */}
-      <Gallery />
-    </div>
-  );
-}
-
-function Gallery() {
+const Gallery = () => {
+  let data = [
+    {
+      id: 1,
+      imgSrc: Nitrox1,
+    },
+    {
+      id: 2,
+      imgSrc: Nitrox2,
+    },
+    {
+      id: 3,
+      imgSrc: Nitrox5,
+    },
+    {
+      id: 4,
+      imgSrc: Nitrox6,
+    },
+    {
+      id: 5,
+      imgSrc: Nitrox8,
+    },
+    {
+      id: 6,
+      imgSrc:Nitrox9,
+    },
+    {
+      id: 7,
+      imgSrc: Nitrox10,
+    },
+    {
+      id: 8,
+      imgSrc: Nitrox11,
+    },
+    {
+      id: 9,
+      imgSrc: Nitrox12,
+    },
+    {
+      id: 10,
+      imgSrc: Nitrox13,
+    },
+    {
+      id: 11,
+      imgSrc: Nitrox11,
+    },
+    {
+      id: 12,
+      imgSrc: Nitrox12,
+    },
+    {
+      id: 13,
+      imgSrc: Nitrox13,
+    },
+    {
+      id: 14,
+      imgSrc: Nitrox15,
+    },
+    {
+      id: 15,
+      imgSrc: Nitrox16,
+    },
+    {
+      id: 16,
+      imgSrc: Nitrox17,
+    },
+    {
+      id: 17,
+      imgSrc: Nitrox18,
+    },
+    {
+      id: 18,Nitrox20,
+    },
+    {
+      id: 19,
+      imgSrc: Nitrox21,
+    },
+    {
+      id: 20,
+      imgSrc: Nitrox23,
+    },
+    {
+      id: 21,
+      imgSrc: Nitrox25,
+    },
+    {
+      id: 22,
+      imgSrc: Nitrox26,
+    },
+    {
+      id: 23,
+      imgSrc: Nitrox36,
+    },
+    {
+      id: 24,
+      imgSrc: Nitrox61,
+    },
+    {
+      id: 25,
+      imgSrc: Nitrox62,
+    },
+    {
+      id: 26,
+      imgSrc: Nitrox65,
+    },
+    {
+      id: 27,
+      imgSrc: Nitrox66,
+    },
+    {
+      id: 28,
+      imgSrc: Nitrox71,
+    },
+    {
+      id: 29,
+      imgSrc: Nitrox75,
+    },
+    {
+      id: 30,
+      imgSrc: Nitrox78,
+    },
+    
+  ];
+  const [model, setModel] = useState(false);
+  const [tempimgSrc, setTempImgSrc] = useState("");
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+  };
   return (
     <>
-      <div className="Gallery-row">
-        <div className="Gallery-column">
-          <img src={Nitrox1} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox2} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox5} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox6} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox8} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox9} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox10} alt="hello" className="image_of_gallery" />
+      <div className="galleryContainer">
+        <div className={model ? "model open" : "model"}>
+          <img src={tempimgSrc} onClick={() => setModel(false)} />
+          {/* <CloseIcon  onClick={()=> setModel(false) }/> */}
         </div>
-        <div className="Gallery-column">
-          <img src={Nitrox11} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox12} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox13} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox15} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox16} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox17} alt="hello" className="image_of_gallery" />
-        </div>
-        <div className="Gallery-column">
-          <img src={Nitrox18} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox20} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox21} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox23} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox2} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox25} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox26} alt="hello" className="image_of_gallery" />
-        </div>
-        <div className="Gallery-column">
-          <img src={Nitrox36} className="image_of_gallery"  />
-          <img src={Nitrox61} className="image_of_gallery" />
-          <img src={Nitrox62} className="image_of_gallery" />
-          <img src={Nitrox65} className="image_of_gallery"  />
-          <img src={Nitrox66} className="image_of_gallery" />
-          <img src={Nitrox10} className="image_of_gallery"  />
-        </div>
-        <div className="Gallery-column">
-          <img src={Nitrox10} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox20} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox5} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox9} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox75} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox71} alt="hello" className="image_of_gallery" />
-          <img src={Nitrox78} alt="hello" className="image_of_gallery" />
+        <div className="gallery">
+          {data.map((item, index) => {
+            return (
+              <div
+                className="pics"
+                key={index}
+                onClick={() => getImg(item.imgSrc)}
+              >
+                <img
+                  className="Galleryimg"
+                  src={item.imgSrc}
+                  style={{ width: "100%" }}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
-      <br/><br/>
     </>
   );
-}
-export default App;
+};
+export default Gallery;
