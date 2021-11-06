1980a82155edea1787f210a78525537fd3874898
mittalkeshav999 <89253638+mittalkeshav999@users.noreply.github.com>
2021-11-06 13:54:27 +0530

    Contact u updated as commanded

--- sae-nitkkr/src/Components/ContactUs/ContactUs.js
+++ sae-nitkkr/src/Components/ContactUs/ContactUs.js
@@ -11,36 +11,37 @@ function ContactUS() {
          <h1 className="heading">CONTACT US</h1><br /><br />
             <div className="whole-body">
             <div className="details">
-            <div className="abc" id="abcd" ><img src={location} alt="" />
+            <div className="abc" id="abcd" ><div className="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
                         <a className="location" href="https://www.google.com/maps/dir/29.5070878,75.0449986/WRV9%2BW46+Gol+Canteen+(SAE+NITKKR),+NIT,+Thanesar,+Haryana+136119/@29.7293325,74.8076003,8z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x390e3f5100000001:0x7f8e5219d122c23e!2m2!1d76.8178281!2d29.9447794">  <div className="tt"> Gol,NIT Kurukshetra</div></a>
                     </div>
-                   <div className="abc"> <img src={mail} alt="" />  <div className="tt"> saenitkurukshetra@gmail.com</div></div>
+                   <div className="abc"><div className="icon"> <i class="fa fa-envelope" aria-hidden="true"></i></div>
+ <div className="tt"> saenitkurukshetra@gmail.com</div></div>
                    <br /><br />
-                   <div className="abc"> <img src={nx} alt="" />  <div className="tt"> teamnitrox@gmail.com</div></div>
+                   <div className="abc"> <div className="iconimgnx"><img src={nx} alt="" /> </div> <div className="tt"> teamnitrox@gmail.com</div></div>
                    <br /><br />
-                   <div className="abc"> <img src={ac} alt="" /> <div className="tt"> teamaccelerons@gmail.com</div></div>
+                   <div className="abc"><div className="iconimgacc"> <img src={ac} alt="" /></div> <div className="tt"> teamaccelerons@gmail.com</div></div>
                    </div>
 
                 <div className="container">
                     <br />
-                    <div className="field">Name: <div className="input"><input type="text" alt="Name"  /></div></div>
-                    <div className="field">Email:<div className="input"> <input type="email" name="email" id="" /></div></div>
-                    <div className="field">Mobile No.: <div className="input"><input type="number" name="" id="" /></div></div>
-                    <div className="field">Institute: <div className="input"><input type="text" name="" id="" /></div></div>
-                    <div className="field">Message: <div className="input"><input type="text" name="" id="" /></div></div>
-                    <div className="field">Reasonto contact us:<div className="input"> <select name="--slect any one--" id="s1">
+                    <div className="field"> <input type="text" alt="Name" required="required" /> <span> Name </span></div>
+                    <div className="field"> <input type="email" name="email" id=""required="required" /><span> Email </span></div>
+                    <div className="field"><input type="number" name="" id=""required="required" /><span> Phone </span></div>
+                    <div className="field"><input type="text" name="" id=""required="required" /><span>Institute </span> </div>
+                    <div className="field"> <textarea required="required" name="" id="" cols="65" rows="6"></textarea><span> Message </span></div>
+                    <div className="field">Reason to contact us  <select name="--slect any one--" id="s1">
                         <option value="option">Want to write a blog</option>
                         <option value="option">Sponsership</option>
                         <option value="option">Brand Ambassador</option>
                         <option value="option">Fault in content on Website</option>
-                    </select></div></div>
-                    <div className="field">Person contacting us is: <div className="input"><select name="--select any one--" id="s2">
+                    </select></div>
+                    <div className="field">Person contacting us is <select name="--select any one--" id="s2">
                         <option value="option">Club Alumni</option>
                         <option value="option">Company Representative</option>
                         <option value="option">Student</option>
                         <option value="option">Proffesor</option>
-                    </select></div></div>
-                    <div className="button"><button>Submit </button></div>
+                    </select></div>
+                    <div className="button"><button> Submit </button></div>
                 </div>
             </div>
 
--- sae-nitkkr/src/Containers/HomePage/homePage.js
+++ sae-nitkkr/src/Containers/HomePage/homePage.js
@@ -8,6 +8,7 @@ import ExploreUs from '../../Containers/HomePage/ExploreUs/ExploreUs';
 import ProfIncharges from '../../Containers/HomePage/ProfInchargesAndManagers/ProfIncharges';
 import Managers from '../../Containers/HomePage/ProfInchargesAndManagers/Managers';
 import Footer from '../../Components/Footer/Footer(black)/FooterBlack';
+// import ContactUs from '../../Components/ContactUs/ContactUs'
 
 function HomePage(){
 
@@ -25,6 +26,7 @@ function HomePage(){
             <ExploreUs/>
             <ProfIncharges/>
             <Managers/>
+            {/* <ContactUs/> */}
             <Footer/>
         </>
     )
