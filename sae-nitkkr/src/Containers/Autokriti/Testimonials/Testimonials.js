ea5af4d2b84303abb0a4739919d6fa67e4293b61
mittalkeshav999 <89253638+mittalkeshav999@users.noreply.github.com>
2021-12-10 09:34:18 +0530

    New Testimonials uploaded

--- sae-nitkkr/src/Containers/Autokriti/Testimonials/Testimonials.js
+++ sae-nitkkr/src/Containers/Autokriti/Testimonials/Testimonials.js
@@ -6,34 +6,31 @@ function Testimonials() {
     <div className="Testimonials-body">
       <div className="Testimonials-vertical"></div> <br />
       <div className="Testimonials-head">TESTIMONIALS</div>
-      <div className="Testimonials-vertical"></div><br/>
-      <div className="Testimonials-cards">
-        <div className="Testimonials-cards13"></div>
-        <div className="Testimonials-card2">
-          <div className="Testimonials-text"> <br />
-            Quite an informative workshop with knowledgeable people. They tauht with a lot of enthusiasm
-            <br /> <br /> 
-            <div className="Testimonials-byname"><br/>
-              <p id="Testimonials-nameChangedToBlack">-Sakshi</p>
-            </div>
-          </div>
+      <div className="Testimonials-vertical"></div><br/> <br /> <br /> <br />
+     <div className="cards">
+       <div className="card1">
+        <div className="text">Quite an informative workshop with knowledgeable people. They tauht with a lot of enthusiasm
+         <br /> <br />
+        <div className="name">-Sakshi</div>
         </div>
-        <div className="Testimonials-card13">
-          <div className="Testimonials-card1">
-            <div className="Testimonials-text"> <br />
-            A lot of "hands on" experience and clear explanations of car dynamics  <br/><br />{" "}
-              <br/> <div className="Testimonials-byname"><br/>-Anudeep Sethi</div>
-            </div>
-          </div>
-          <div className="Testimonials-card3">
-            <div className="Testimonials-text">
-              <br />
-              The workshop was fun, especially the display of Porche Cayenne and talented guests of honour.<br/> <br/>{" "}
-              <br/> <div className="Testimonials-byname"><br/>-Abhishek Goel</div>
-            </div>
-          </div>
+        
+       </div>
+       <div className="card23">
+       <div className="card2">
+        <div className="text">A lot of "hands on" experience and clear explanations of car dynamics
+        <br /> <br />
+        <div className="name">-Anudeep Sethi</div>
         </div>
-      </div>
+        
+       </div>
+       <div className="card3">
+        <div className="text">The workshop was fun, especially the display of Porche Cayenne and talented guests of honour.
+        <br /> <br /> 
+        <div className="name">-Abhishek Goel</div>
+        </div>
+       
+       </div></div> 
+     </div>
     </div>
   );
 }
