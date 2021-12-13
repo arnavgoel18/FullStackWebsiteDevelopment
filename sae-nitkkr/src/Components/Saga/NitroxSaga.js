7d1bfdaed55d1d5e16099cdafcefefe4d25c9794
Navneet-Raj99 <89241731+Navneet-Raj99@users.noreply.github.com>
2021-12-14 00:29:40 +0530

    Nitrox Saga (Accelerons saga not up to the mark)

--- sae-nitkkr/src/Components/Saga/NitroxSaga.js
+++ sae-nitkkr/src/Components/Saga/NitroxSaga.js
@@ -24,80 +24,80 @@ function Saga(){
 
     const nitroxInfo = [
         {
-            competition1Name: "KEY UPGRADES",
+            competition1Name: "ATVC",
             competition2Name: "BAJA SAE India",
         },
         {
             id: 2011,
             atvc: {
-                achievement1: "AIR1 in Terrain Response{}",
-                achievement2: "AIR2 in VD{}",
-                achievement3: "AIR3 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "First Time NIT KURUKSHETRA's DREAM ROLLED ON WHEELS",
-                achievement2: "AIR6 in VD{}",
-                achievement3: "AIR7 in Stability{}",
+                achievement2: "",
+                achievement3: "",
             }
         },
         {
             id: 2012,
             atvc: {
-                achievement1: "IMPROVED IN SUSPENSION",
-                achievement2: "AIR2 in VD{}",
-                achievement3: "AIR3 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "First Time VIRTUAL CLEARED Roared On BAJA Tracks in -Hill Climb,Endurance,Accleration",
-                achievement2: "AIR6 in VD{}",
-                achievement3: "AIR7 in Stability{}",
+                achievement2: "",
+                achievement3: "",
             }
         },
         {
             id: 2013,
             atvc: {
-                achievement1: "Tuned Acceleration",
-                achievement2: "High Top Speed",
-                achievement3: "AIR3 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "AIR8 in Endurance",
                 achievement2: "AIR10 Out of 262 TEAMS",
-                achievement3: "AIR7 in Stability{}",
+                achievement3: "",
             }
         },
         {
             id: 2014,
             atvc: {
-                achievement1: "Presented the Innovative Idea Of CUTTING BRAKES",
-                achievement2: "AIR2 in VD{}",
-                achievement3: "AIR3 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "AIR20 Out Of 327 TEAMS",
-                achievement2: "AIR2 in VD{}",
-                achievement3: "AIR7 in Stability{}",
+                achievement2: "",
+                achievement3: "",
             }
         },
         {
             id: 2015,
             atvc: {
                 achievement1: "Improved Suspension",
-                achievement2: "AIR2 in VD{}",
-                achievement3: "AIR3 in Stability{}",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "AIR3 in Suspension and Traction Event",
                 achievement2: "AIR7 out of 397 TEAMS",
-                achievement3: "AIR7 in Stability{}",
+                achievement3: "",
             }
         },
         {
             id: 2016,
             atvc: {
-                achievement1: "Significant Weight Reduction. ",
-                achievement2: "Most Cost Efficient Car Yet",
-                achievement3: "Self Designed Gear Box",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "AIR2 in Cost Efficiency.",
@@ -108,35 +108,35 @@ function Saga(){
         {
             id: 2017,
             atvc: {
-                achievement1: "Ergonomic Sling Seat Design.",
-                achievement2: "Innovated Safety Belts System.",
-                achievement3: "AIR3 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
                 achievement1: "AIR29 out of 400 TEAMS",
                 achievement2: "AIR7 out of 397 TEAMS",
-                achievement3: "AIR7 in Stability{}",
+                achievement3: "",
             }
         },
         {
             id: 2018,
             atvc: {
-                achievement1: "Improved Speed with AIR6(in ATVC) in the same category",
-                achievement2: "Greater Cost Efficiency",
-                achievement3: "Weight Reduction with AIR5(in ATVC) in the same category",
+                achievement1: "AIR5 in Light Weight Category",
+                achievement2: "AIR6 as a Fast Vehicle",
+                achievement3: "",
             },
             baja: {
                 achievement1: "AIR4 in Endurance Run",
                 achievement2: "AIR5 in Cost Efficieny",
-                achievement3: "AIR5 in Light Weight Category",
-                achievement4: "AIR5 in Fast Vehicle Category",
+                achievement3: "",
+                achievement4: "",
             }
         },
         {
             id: 2019,
             atvc: {
-                achievement1: "Improved Acceleration",
-                achievement2: "Improved Design",
+                achievement1: "AIR1 in Build Quality",
+                achievement2: "AIR1 in Light Weight Category",
                 achievement3: "AIR1 in Engineering Design",
                 achievement4: "AIR1 in Innovation",
                 achievement5: "AIR3 in Suspension And Traction",
@@ -155,14 +155,14 @@ function Saga(){
         {
             id: 2020,
             atvc: {
-                achievement1: "AIR1 in Desgin{}",
-                achievement2: "AIR2 in VD{}",
-                achievement3: "AIR3 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             },
             baja: {
-                achievement1: "AIR5 in Desgin{}",
-                achievement2: "AIR6 in VD{}",
-                achievement3: "AIR7 in Stability{}",
+                achievement1: "",
+                achievement2: "",
+                achievement3: "",
             }
         }
         
@@ -189,7 +189,7 @@ function Saga(){
             setcomp1Achievements(comp1Achievements => []);
             setcomp2Achievements(comp2Achievements => []);
 
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[2].atvc.achievement1]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[2].atvc.achievement1]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[2].atvc.achievement2]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[2].atvc.achievement3]);
 
@@ -203,8 +203,8 @@ function Saga(){
             setcomp1Achievements(comp1Achievements => []);
             setcomp2Achievements(comp2Achievements => []);
 
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[3].atvc.achievement1]);
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[3].atvc.achievement2]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[3].atvc.achievement1]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[3].atvc.achievement2]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[3].atvc.achievement3]);
 
             setcomp2Achievements(comp2Achievements => [...comp2Achievements, nitroxInfo[3].baja.achievement1]);
@@ -217,7 +217,7 @@ function Saga(){
             setcomp1Achievements(comp1Achievements => []);
             setcomp2Achievements(comp2Achievements => []);
 
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[4].atvc.achievement1]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[4].atvc.achievement1]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[4].atvc.achievement2]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[4].atvc.achievement3]);
 
@@ -231,7 +231,7 @@ function Saga(){
             setcomp1Achievements(comp1Achievements => []);
             setcomp2Achievements(comp2Achievements => []);
 
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[5].atvc.achievement1]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[5].atvc.achievement1]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[5].atvc.achievement2]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[5].atvc.achievement3]);
 
@@ -245,9 +245,9 @@ function Saga(){
             setcomp1Achievements(comp1Achievements => []);
             setcomp2Achievements(comp2Achievements => []);
 
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[6].atvc.achievement1]);
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[6].atvc.achievement2]);
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[6].atvc.achievement3]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[6].atvc.achievement1]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[6].atvc.achievement2]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[6].atvc.achievement3]);
 
             setcomp2Achievements(comp2Achievements => [...comp2Achievements, nitroxInfo[6].baja.achievement1]);
             setcomp2Achievements(comp2Achievements => [...comp2Achievements, nitroxInfo[6].baja.achievement2]);
@@ -259,8 +259,8 @@ function Saga(){
             setcomp1Achievements(comp1Achievements => []);
             setcomp2Achievements(comp2Achievements => []);
 
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[7].atvc.achievement1]);
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[7].atvc.achievement2]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[7].atvc.achievement1]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[7].atvc.achievement2]);
             // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[7].atvc.achievement3]);
 
             setcomp2Achievements(comp2Achievements => [...comp2Achievements, nitroxInfo[7].baja.achievement1]);
@@ -276,7 +276,7 @@ function Saga(){
 
             setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[8].atvc.achievement1]);
             setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[8].atvc.achievement2]);
-            setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[8].atvc.achievement3]);
+            // setcomp1Achievements(comp1Achievements => [...comp1Achievements, nitroxInfo[8].atvc.achievement3]);
 
             setcomp2Achievements(comp2Achievements => [...comp2Achievements, nitroxInfo[8].baja.achievement1]);
             setcomp2Achievements(comp2Achievements => [...comp2Achievements, nitroxInfo[8].baja.achievement2]);
@@ -375,9 +375,9 @@ function Saga(){
                             <p>
                                 
                                 <ul>
-                                <li id="atvc_li1">AIR1 in Desgin</li>
+                                {/* <li id="atvc_li1">AIR1 in Desgin</li>
                                     <li id="atvc_li2">AIR2 in VD</li>
-                                    <li  id="atvc_li3">AIR3 in Stability</li>
+                                    <li  id="atvc_li3">AIR3 in Stability</li> */}
                                  
                                     {
                                     comp2Achievements.map(position => 
@@ -394,9 +394,9 @@ function Saga(){
                             <h4 id="nitrox_competition1">{nitroxInfo[0].competition1Name}</h4>
                             <p>
                                 <ul>
-                                <li  id="baja_li1">AIR5 in Desgin</li>
+                                {/* <li  id="baja_li1">AIR5 in Desgin</li>
                                     <li  id="baja_li2">AIR6 in VD</li>
-                                    <li  id="baja_li3">AIR7 in Stability</li>
+                                    <li  id="baja_li3">AIR7 in Stability</li> */}
                                 {comp1Achievements.map(position => 
                                         (
                                             
