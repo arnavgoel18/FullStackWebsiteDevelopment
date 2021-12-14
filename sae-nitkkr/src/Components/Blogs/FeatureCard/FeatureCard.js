ccf95a44514a758aa4951796e1aa7548e87cc855
mittalkeshav999 <89253638+mittalkeshav999@users.noreply.github.com>
2021-12-14 17:36:28 +0530

    FeatureCard

---
+++ sae-nitkkr/src/Components/Blogs/FeatureCard/FeatureCard.js
@@ -0,0 +1,14 @@
+import './FeatureCard.css';
+
+function FeatureCard() {
+  return (
+    <div className="App">
+    <div className="feature_container">
+    <div className="text"> Want to Feature your own blog ? </div>
+    <div className="button"><button>Learn More</button></div>
+    </div>
+    </div>
+  );
+}
+
+export default FeatureCard;
