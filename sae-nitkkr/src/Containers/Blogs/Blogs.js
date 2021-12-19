bbdb58bddd30c5451df643a1520d88e6b0798528
arnavgoel18 <arnav18.goel@gmail.com>
2021-12-19 13:08:09 +0530

    Footers

---
+++ sae-nitkkr/src/Containers/Blogs/Blogs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Footer from '../../Components/Footer/Footer(black)/FooterBlack'
+import NavBar from '../../Components/NavBar/NavBar'
+
+function Blogs(){
+    <>
+        <NavBar/>
+        <div className="main">
+
+        </div>
+        <Footer/>
+
+    </>
+}
+
+export default Blogs;
