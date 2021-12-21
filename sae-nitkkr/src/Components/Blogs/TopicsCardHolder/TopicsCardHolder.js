d8f2428fe8a714a75eb7865b0a8d296b363a1006
kritika agrawal <78923214+Kritikaagra@users.noreply.github.com>
2021-12-21 14:03:00 +0530

    new topicCardHolder code added

--- sae-nitkkr/src/Components/Blogs/TopicsCardHolder/TopicsCardHolder.js
+++ sae-nitkkr/src/Components/Blogs/TopicsCardHolder/TopicsCardHolder.js
@@ -1,63 +1,47 @@
+import React from 'react'
 import './TopicsCardHolder.css'
-import React from "react";
-import image from '.topiccardholder1/.png'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-import { faShare } from "@fortawesome/free-solid-svg-icons";
-
-function App() {
+// import daq from '../src/Cards Images/daq.png'
+// import brakes from '../src/Cards Images/brakes.png'
+function TopicsCardHolder(props) {
   return (
-    <>
-      <div className="TopicsCardHolder-container" style={{paddingleft:"40px"}}>
-        <Topic image={image} title1="DAQ and " title2="Multicontrollers" date="10101010" />
-        <Topic image={image} title1="DAQ and " title2="Multicontrollers" date="" />
-    
+    <div id="card_container">
+      <div id="card-image">
+          <img src={props.srcs} id="daq_blog_image"/>
       </div>
-
-      <div className="TopicsCardHolder-container" style={{paddingleft:"40px"}}>
-        <Topic image={image} title1="DAQ and " title2="Multicontrollers" date="" />
-        <Topic image={image} title1="DAQ and " title2="Multicontrollers" date="" />
-    
+      <div id="card-title">
+          {props.title}
       </div>
-
-     
-    </>
-  );
+      {/* <img src="https://img.icons8.com/ios/50/000000/forward-arrow.png"/> */}
+      <div id="card-actions">
+          <div id="shareincard">
+              <div id="shareincard_image">
+              <a href='/home'>
+             <img
+              id="blog_embed_shareimage"
+              alt="clap_embed_shareimage"
+              src="https://img.icons8.com/ios/50/000000/forward-arrow.png"/>
+              </a>
+              </div>
+              <div id="shareincard_title">
+               Share
+              </div>
+              {/* <img src="https://img.icons8.com/material-outlined/50/000000/planner.png"/> */}
+          </div>
+          <div id="card_date">
+          <div id="card_date_image">
+              <a href='/home'><img
+              id="blog_embed_calender"
+              alt="blog_embed_calender"
+              src="https://img.icons8.com/material-outlined/50/000000/planner.png"/>
+              </a>
+              </div>
+              <div id="card_date_title">
+               3/09/21
+              </div>
+          </div>
+      </div>
+    </div>
+  )
 }
 
-function Topic(props) {
-  return (
-    <>
-      <body>
-        <div className="xyz">
-        <div className="TopicsCardHolder-card">
-          <img className="TopicsCardHolder-image" src={props.image} alt="" />
-
-          <h1 className="TopicsCardHolder-title">
-            <b>{props.title1}</b>
-            <br />
-            <b>{props.title2}</b>
-          </h1>
-
-          <footer className="TopicsCardHolder-footer">
-            <ul className="TopicsCardHolder-footer-list">
-              <li>
-                <FontAwesomeIcon className="TopicsCardHolder-fontawesome" icon={faShare} />{" "}
-                &nbsp; Share
-              </li>
-              <li >
-                <FontAwesomeIcon className="TopicsCardHolder-fontawesome1"  style={{marginleft:"10px"}} icon={faCalendar}  />
-        
-              </li>
-              <li style={{float:"right"}}>{props.date}</li>
-            </ul>
-          </footer>
-        </div>
-        </div>
-      </body>
-    </>
-  );
-}
-
-export default App;
+export default TopicsCardHolder
