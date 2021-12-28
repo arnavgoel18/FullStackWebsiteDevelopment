eee05c345ed9691ffb635c0c8078de4f72c39565
arnavgoel18 <arnav18.goel@gmail.com>
2021-12-28 11:54:46 +0530

    Update Ambassador.js

--- sae-nitkkr/src/Containers/Ambassador/Ambassador.js
+++ sae-nitkkr/src/Containers/Ambassador/Ambassador.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
 import AmbassadorBenefits from './AmbassadorBenefits/AmbassadorBenefits'
+import AmbassadorRoles from './AmbassadorRoles/AmbassadorRoles'
 import Footer from '../../Components/Footer/Footer(black)/FooterBlack'
+
 
 function Ambassador(){
     return(
         <>
             <NavBar/>
             <AmbassadorBenefits/>
+            <AmbassadorRoles/>
             <Footer/>
         </>
     )
