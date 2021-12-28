3442f6c2de6036149935dffca02d9422e4eca211
Prikshit-anthal <prikshit_12013102@nitkkr.ac.in>
2021-12-28 21:32:29 +0530

    AmbassadorLander page added

---
+++ sae-nitkkr/src/Containers/Ambassador/AmbassadorLander/AmbassadorLander.js
@@ -0,0 +1,39 @@
+import './BecomeOurAmbassador.css'
+
+function BecomeOurAmbassador() {
+  return (
+    <div id='Main'>
+      <div className='Topborder'></div>
+
+      <div className='Ambassadortext'>
+        <div id='Text'>
+          <h1>Become our Ambassador</h1>
+          <div id='Body'>
+            We hope to welcome you to our SAE Family as an esteemed ambassdor to
+            our organisation. Becoming the ambassador can give you exclusive
+            rights to our courses, workshops and other benefits. You will help
+            us recruit and manage applicants, act as intermediaries between SAE
+            NITKKR and appliants from your college, and much more! Become a
+            Campus Ambassador today for the leading automotive workshop in North
+            India
+          </div>
+        </div>
+
+        <div className='Circle'>
+          <div className='Circletext'>
+            <b>e-Autokriti</b>
+          </div>
+        </div>
+      </div>
+      {/* 
+        <div className='Bottomborder'>
+          <div>
+            <a>Learn More</a> about the Benefits and Roles of a Campus
+            Ambassador before Signing Up
+          </div>
+        </div>
+         */}
+    </div>
+  )
+}
+export default BecomeOurAmbassador
