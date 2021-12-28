e4cc456082be6da6876f5714dda80b36708ee923
kritika <kritikaagrawal75@gmail.com>
2021-12-28 11:35:02 +0530

    change css of benefits card

--- sae-nitkkr/src/Containers/Ambassador/AmbassadorBenefits/AmbassaderBenefitsCard.js
+++ sae-nitkkr/src/Containers/Ambassador/AmbassadorBenefits/AmbassaderBenefitsCard.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import "./AmbassaderBenefitsCard.css";
 
 function AmbassadorBenefitsCard(props) {
   return (
     <div className="benefitsBody">
-        <div className='upperPart'>
-     <div className='number'>{props.number}</div>
-     <h3 className='benefitHeading'>{props.heading}</h3>
-     </div>
-     <div className='benefitContent'>{props.content}</div>
+      <div className="upperPart">
+        <div className="number">{props.number}</div>
+        <h3 className="benefitHeading">{props.heading}</h3>
+      </div>
+      <div className="benefitContent">{props.content}</div>
     </div>
   );
 }
