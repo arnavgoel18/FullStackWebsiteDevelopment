deb172c88179bd06f0a569eadf96afe71f0bc5a3
arnavgoel18 <arnav18.goel@gmail.com>
2021-12-28 12:30:44 +0530

    Update Ambassador.js

--- sae-nitkkr/src/Containers/Ambassador/Ambassador.js
+++ sae-nitkkr/src/Containers/Ambassador/Ambassador.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
+import AmbassadorLander from './AmbassadorLander/AmbassadorLander'
+import Form from './FormDIV/FormDIV'
 import AmbassadorBenefits from './AmbassadorBenefits/AmbassadorBenefits'
 import AmbassadorRoles from './AmbassadorRoles/AmbassadorRoles'
+import RedirectToForm from './RedirectToForm/RedirectToForm'
+import RedirectToInfo from './RedirectToInfo/RedirectToInfo'
 import Footer from '../../Components/Footer/Footer(black)/FooterBlack'
 
 
@@ -9,8 +13,13 @@ function Ambassador(){
     return(
         <>
             <NavBar/>
+            {/* <AmbassadorLander/> */}
+            {/* <RedirectToInfo/> */}
+            {/* <Form/> */}
             <AmbassadorBenefits/>
+            {/* <RedirectToForm/> */}
             <AmbassadorRoles/>
+            {/* <RedirectToForm/> */}
             <Footer/>
         </>
     )
