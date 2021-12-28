6996b4923614ccf08e79e1fcc1be4932ea6adadf
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2021-12-28 15:52:16 +0530

    add FormDIV

--- sae-nitkkr/src/Containers/Ambassador/Ambassador.js
+++ sae-nitkkr/src/Containers/Ambassador/Ambassador.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
 import AmbassadorLander from './AmbassadorLander/AmbassadorLander'
-import Form from './FormDIV/FormDIV'
 import AmbassadorBenefits from './AmbassadorBenefits/AmbassadorBenefits'
 import AmbassadorRoles from './AmbassadorRoles/AmbassadorRoles'
 import RedirectToForm from './RedirectToForm/RedirectToForm'
 import RedirectToInfo from './RedirectToInfo/RedirectToInfo'
 import Footer from '../../Components/Footer/Footer(black)/FooterBlack'
-
+import FormDIV from './FormDIV/FormDIV'
 
 function Ambassador(){
     return(
@@ -15,7 +14,7 @@ function Ambassador(){
             <NavBar/>
             {/* <AmbassadorLander/> */}
             <RedirectToInfo/>
-            {/* <Form/> */}
+            <FormDIV/>
             <AmbassadorBenefits/>
             <RedirectToForm/>
             <AmbassadorRoles/>
