a2117851e9e6998d7740ed1688ef75a19559e523
arnavgoel18 <arnav18.goel@gmail.com>
2022-01-01 15:45:54 +0530

    Update DisplayAmbassador.js

--- sae-nitkkr/src/Containers/Backend/DisplayAmbassador/DisplayAmbassador.js
+++ sae-nitkkr/src/Containers/Backend/DisplayAmbassador/DisplayAmbassador.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Redirect } from "react-router-dom";
 import DisplayCard from './DisplayCard'
 import db from '../../../Firebase.js'
 import { collection, getDocs, Timestamp, doc, setDoc } from 'firebase/firestore'
@@ -8,8 +9,6 @@ function DisplayInfo() {
   var [index, setIndex] = useState(0)
   var detailList=[]
   
-
-  const [forreload, setForreload] = useState([])
   var [detail, setDetail] = useState({})
 
   //Get Information from Firebase into detailList array
@@ -21,8 +20,6 @@ function DisplayInfo() {
     
     setDetail(detailList[index])
     
-    console.log(detailList);
-
     return detailList
   }
    
@@ -35,11 +32,23 @@ function DisplayInfo() {
     setIndex(--index);
     console.log(index);
   }
-  
-  //use effect to get info 
-  useEffect(() => {
-    setForreload([...forreload, getInfo()])
-   }, [])
+
+  window.addEventListener('load', getInfo());
+
+  const token=localStorage.getItem("token");
+
+  let loggedin=true;
+  if(token==null)
+  {
+    loggedin=false;
+  }
+
+
+  if(loggedin==false)
+  {
+    return <Redirect to="/admin/login"/>
+  }
+  else{
 
   return (
     <div className='displayDiv'>
@@ -49,24 +58,24 @@ function DisplayInfo() {
           <div>{index}</div>
           <button onClick={increment}>+</button>
         </div>
-        
-        <DisplayCard
-              key={index}
-              FullName={detail.studentName}
-              ClgName={detail.collegeName}
-              Branch={detail.branch}
-              CurrentSem={detail.semester}
-              Phoneno={detail.phone}
-              Emailid={detail.email}
-              s1={detail.s1}
-              s2={detail.s2}
-              ans1={detail.longAnswer1}
-              ans2={detail.longAnswer2}
-              ans3={detail.longAnswer3}
-            />
+          <DisplayCard
+            key={index}
+            FullName={detail.studentName}
+            ClgName={detail.collegeName}
+            Branch={detail.branch}
+            CurrentSem={detail.semester}
+            Phoneno={detail.phone}
+            Emailid={detail.email}
+            s1={detail.s1}
+            s2={detail.s2}
+            ans1={detail.longAnswer1}
+            ans2={detail.longAnswer2}
+            ans3={detail.longAnswer3}
+          />
       </div>
     </div>
   )
+  }
 }
 
 export default DisplayInfo
