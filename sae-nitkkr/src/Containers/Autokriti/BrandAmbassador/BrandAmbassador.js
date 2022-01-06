3c26221de55d515ee02816ff1ac446328ef7fb97
sourabh707 <buckalsourabh@gmail.com>
2022-01-06 21:25:19 +0530

    brand ambassador

--- sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/BrandAmbassador.js
+++ sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/BrandAmbassador.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import "./BrandAmbassador.css";
-
+import xyz from '../../../Assets/BrandAmbassador/BrandAmbassador.jpeg'
 function BrandAmbassador() {
   return (
-    <div className="BrandAmbassador-body">
+    <div>
+    {/* <div className="BrandAmbassador-body">
       <div className="BrandAmbassador-boxes">
         <div className="BrandAmbassador-box1">
           <div className="BrandAmbassador-box11">
@@ -35,6 +36,19 @@ function BrandAmbassador() {
           <button className="BrandAmbassador-box4">JOIN US</button>
         </Link>
       </div>
+    </div> */}
+    <div className="BrandAmbassador-container">
+      <div className="BrandAmbassador-text">
+          <div className="BrandAmbassador-title">BECOME <br/> OUR BRAND AMBASSADOR</div>
+          <div >Spread a word about us in your college and get amazing perks!</div>
+          <Link to="/ambassador">
+          <button className="BrandAmbassador-button">JOIN US</button>
+          </Link>
+      </div>
+      <div >
+          <img  className="BrandAmbassador-image" src={xyz} />
+      </div>
+    </div>
     </div>
   );
 }
