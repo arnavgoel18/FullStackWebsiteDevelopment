8021db5d3e126ad3f1e4f7a0808f8ed1a07f9260
Navneet-Raj99 <89241731+Navneet-Raj99@users.noreply.github.com>
2022-01-07 14:23:44 +0530

    First Display AutoKriti Carousal

---
+++ sae-nitkkr/src/Components/FirstDisplayDIV/Autokriti/Slider.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+import './Slider.css'
+import BtnSlider from './BtnSlider'
+import dataSlider from './dataSlider'
+
+export default function Slider() {
+
+    const [slideIndex, setSlideIndex] = useState(1)
+
+    const nextSlide = () => {
+        if(slideIndex !== dataSlider.length){
+            setSlideIndex(slideIndex + 1)
+        } 
+        else if (slideIndex === dataSlider.length){
+            setSlideIndex(1)
+        }
+    }
+    setTimeout(() => {
+        nextSlide();
+    }, 2000);
+
+    function prevSlide () {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1){
+            setSlideIndex(dataSlider.length)
+        }
+    }
+
+    function moveDot(index) {
+        setSlideIndex(index)
+    }
+
+    return (
+        <div className="container-slider">
+            {dataSlider.map((obj, index) => {
+                return (
+                    <div
+                    key={obj.id}
+                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+                    >
+                        <img 
+                        src={ `Imgs/Autokriti${index + 1}.jpeg`} 
+                        />
+                    </div>
+                )
+            })}
+            <BtnSlider moveSlide={nextSlide} direction={"next"} />
+            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+
+            <div className="container-dots">
+                {Array.from({length: 5}).map((item, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ sae-nitkkr/src/Components/FirstDisplayDIV/Autokriti/dataSlider.js
@@ -0,0 +1,31 @@
+
+
+const dataSlider = [
+  {
+    id: 1,
+    title: "Autokriti",
+    type: "Workshop"
+  },
+  {
+    id: 2,
+    title: "Autokriti",
+    type: "Workshop"
+  },
+  {
+    id: 3,
+    title: "Autokriti",
+    type: "Workshop"
+  },
+  {
+    id: 4,
+    title: "Autokriti",
+    type: "Workshop"
+  },
+  {
+    id:5,
+    title: "Autokriti",
+    type: "Workshop"
+  },
+];
+
+export default dataSlider;
