2d9c64347bc43a92408bc114f161634e0915413c
arnavgoel18 <arnav18.goel@gmail.com>
2022-01-11 22:33:24 +0530

    Ambassador Added Back

---
+++ sae-nitkkr/src/Containers/Ambassador/Ambassador.js
@@ -0,0 +1,32 @@
+import React, { Component, useEffect} from 'react'
+import NavBar from '../../Components/NavBar/NavBar'
+import AmbassadorLander from './AmbassadorLander/AmbassadorLander'
+import AmbassadorBenefits from './AmbassadorBenefits/AmbassadorBenefits'
+import AmbassadorRoles from './AmbassadorRoles/AmbassadorRoles'
+import RedirectToForm from './RedirectToForm/RedirectToForm'
+import RedirectToInfo from './RedirectToInfo/RedirectToInfo'
+import Footer from '../../Components/Footer/Footer(black)/FooterBlack'
+import FormDIV from './FormDIV/FormDIV'
+
+function Ambassador(){
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+      }, [])
+
+    return(
+        <>
+            <NavBar/>
+            <AmbassadorLander/>
+            <RedirectToInfo/>
+            <FormDIV/>
+            <AmbassadorBenefits/>
+            <RedirectToForm/>
+            <AmbassadorRoles/>
+            <RedirectToForm/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Ambassador
