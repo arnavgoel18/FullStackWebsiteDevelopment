0808e2ea0334f786adf00da2cb709fa94c2ecabb
playstargopal <gopal_12017079@nitkkr.ac.in>
2022-01-16 12:38:19 +0530

    new code for manager

--- sae-nitkkr/src/Containers/HomePage/ProfInchargesAndManagers/ProfIncharges.js
+++ sae-nitkkr/src/Containers/HomePage/ProfInchargesAndManagers/ProfIncharges.js
@@ -1,17 +1,18 @@
 import './ProfInchargesAndManagers.css';
+import './pm.css';
 import PersonCard from './PersonCard';
 import prof1 from '../../../Assets/ProfIncharges/mittal.jpg';
 import prof2 from '../../../Assets/ProfIncharges/JDSir.jpg';
 import prof3 from '../../../Assets/ProfIncharges/angra.jpg';
 
-function ProfIncharges(){
-    return(
+function ProfIncharges() {
+    return (
         <>
             {/*Professor Incharges*/}
-            <div className = "ProfInchargesandManagers-profIncharge_container">
+            <div className="ProfInchargesandManagers-profIncharge_container">
                 <p className="ProfInchargesandManagers-heading">Professor Incharges</p>
-                
-                <div className = "ProfInchargesandManagers-persons_container">
+
+                <div className="ProfInchargesandManagers-persons_container">
                     <PersonCard name={"Dr. Vinod Mittal"} designation={"Professor"} url={prof1}/>
                     <PersonCard name={"Dr. Jaideep Gupta"} designation={"Professor"} url={prof2}/>
                     <PersonCard name={"Dr. Surjeet Angra"} designation={"Professor"} url={prof3}/>
