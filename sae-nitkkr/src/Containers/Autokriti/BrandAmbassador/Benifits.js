2cb715251d5726a19498fe5167f859a9c04dc0b1
kritika <kritikaagrawal75@gmail.com>
2022-01-17 11:26:54 +0530

    add arrows in amb carousel

--- sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/Benifits.js
+++ sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/Benifits.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './Benifits.css';
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 function Benifits() {
   var imgNames = ['monetaryIncentives', 'workshopsCommissions', 'marketingManagamenet', 'freeAccess', 'communicationSkills']
@@ -31,20 +32,46 @@ function Benifits() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setSlideIndex((preValue) => {
-        if (preValue < imgNames.length - 1) return preValue + 1
-        else return 0
+        if (preValue < imgNames.length - 1){
+           return preValue + 1
+          }
+        else{
+          return 0}
       })
-    }, 5000);
+    }, 4000);
 
     return () => clearTimeout(timer)
   })
+
+  function leftarrow(){
+    if(slideIndex>0){
+    setSlideIndex(slideIndex-1);
+    }
+    else{
+      setSlideIndex(imgNames.length-1);
+    }
+  }
+
+  function rightarrow(){
+    if(slideIndex<imgNames.length -1){
+    setSlideIndex(slideIndex+1);
+    }
+    else{
+      setSlideIndex(0);
+    }
+  }
+
   return (
     <div className='BenifitsBody'>
       <div className="foto">
         <div className="benifitheading">Benefits of being an Ambassador for e - Autokriti </div>
         <img src={`Imgs/${imgNames[slideIndex]}.jpg`}  alt="not found" />
       </div>
+      <div className='amb-carousel-arrow'>
+      <FaArrowLeft className='amb-arrow' onClick={leftarrow}/>
       <h3>{imgSubtitles[slideIndex].heading}</h3>
+      <FaArrowRight className='amb-arrow' onClick={rightarrow}/>
+      </div>
       <p>{imgSubtitles[slideIndex].data}</p>
     </div>
   );
--- sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/Roles.js
+++ sae-nitkkr/src/Containers/Autokriti/BrandAmbassador/Roles.js
@@ -1,45 +1,67 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
-import './Benifits.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Benifits.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 function Roles() {
-   var imgNames = ['recruit', 'spread', 'claimCertificate']
-   var imgSubtitles = [
-     {
-       heading: '1. Recruiting students for Worskhop',
-       data: 'You will help recruit interested candidates for workshops and courses. A traininig session will provide you with all the info you might need.',
-     },
-     {
-       heading: '2. Spreading the Word of our workshop',
-       data: 'Getting the word out using Social Media platmforms or Campus News Letters to enroll as many students as possible.',
-     },
-     {
-       heading: '3. Claim your Ambassador Certificate',
-       data: 'Must bring at least 2 entries in all the courses and workshops combined to avail the Ambassdor Certificate.',
-     },
-   ]
-  const [slideIndex,setSlideIndex]=useState(0);
-  
-  useEffect(()=>{
-const timer = setTimeout(() => {
-  setSlideIndex((preValue) => {
-    if (preValue < imgNames.length - 1) return preValue + 1
-    else return 0
-  })
-}, 5000);
+  var imgNames = ["recruit", "spread", "claimCertificate"];
+  var imgSubtitles = [
+    {
+      heading: "1. Recruiting students for Worskhop",
+      data: "You will help recruit interested candidates for workshops and courses. A traininig session will provide you with all the info you might need.",
+    },
+    {
+      heading: "2. Spreading the Word of our workshop",
+      data: "Getting the word out using Social Media platmforms or Campus News Letters to enroll as many students as possible.",
+    },
+    {
+      heading: "3. Claim your Ambassador Certificate",
+      data: "Must bring at least 2 entries in all the courses and workshops combined to avail the Ambassdor Certificate.",
+    },
+  ];
+  const [slideIndex, setSlideIndex] = useState(0);
 
-return () => clearTimeout(timer)
-  })
- 
-  return (<>
-    <div className='BenifitsBody'>
-      <div className="foto">
-        <div className="benifitheading">Roles of an Ambassador </div>
-        <img src={`Imgs/${imgNames[slideIndex]}.jpg`} alt="not found" />
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSlideIndex((preValue) => {
+        if (preValue < imgNames.length - 1) return preValue + 1;
+        else return 0;
+      });
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  });
+
+  function leftarrow() {
+    if (slideIndex > 0) {
+      setSlideIndex(slideIndex - 1);
+    } else {
+      setSlideIndex(imgNames.length - 1);
+    }
+  }
+
+  function rightarrow() {
+    if (slideIndex < imgNames.length - 1) {
+      setSlideIndex(slideIndex + 1);
+    } else {
+      setSlideIndex(0);
+    }
+  }
+
+  return (
+    <>
+      <div className="BenifitsBody">
+        <div className="foto">
+          <div className="benifitheading">Roles of an Ambassador </div>
+          <img src={`Imgs/${imgNames[slideIndex]}.jpg`} alt="not found" />
+        </div>
+        <div className="amb-carousel-arrow">
+          <FaArrowLeft className="amb-arrow" onClick={leftarrow} />
+          <h3>{imgSubtitles[slideIndex].heading}</h3>
+          <FaArrowRight className="amb-arrow" onClick={rightarrow} />
+        </div>
+        <p>{imgSubtitles[slideIndex].data}</p>
       </div>
-      <h3>{imgSubtitles[slideIndex].heading}</h3>
-      <p>{imgSubtitles[slideIndex].data}</p>
-    </div>
     </>
   );
 }
