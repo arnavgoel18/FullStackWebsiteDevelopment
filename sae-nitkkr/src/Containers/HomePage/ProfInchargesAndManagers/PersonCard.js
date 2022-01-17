c55efaca7ee0cecdb658d2f7fc74746f236c6858
playstargopal <gopal_12017079@nitkkr.ac.in>
2022-01-17 23:13:16 +0530

    remove icons

--- sae-nitkkr/src/Containers/HomePage/ProfInchargesAndManagers/PersonCard.js
+++ sae-nitkkr/src/Containers/HomePage/ProfInchargesAndManagers/PersonCard.js
@@ -1,5 +1,38 @@
 function PersonCard(props){
-  
+  if(props.page == "professor incharge"){
+
+      return(
+          <>
+        <div className="container_pm">
+           <div className="wrapper">
+             
+              <img src={props.url} alt=""/>
+              <div className="name">
+              {props.name}
+              </div>
+              <div className="designation">
+              {props.designation}
+              </div>
+           </div>
+{/*         
+           <div className="icons">
+              <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+              <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+              <li><a href="#"><span class="fab fa-linkedin " ></span></a></li>
+              <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+           </div> */}
+        </div>
+              {/* <div className = "ProfInchargesandManagers-person">
+                  <div className = "ProfInchargesandManagers-circle">
+                      <img src = {props.url}/>
+                  </div>
+                  <div className = "ProfInchargesandManagers-name">{props.name}</div>
+                  <div className = "ProfInchargesandManagers-designation">{props.designation}</div>
+              </div> */}
+          </>
+      )
+  }
+  else{
     return(
         <>
       <div className="container_pm">
@@ -16,7 +49,7 @@ function PersonCard(props){
       
          <div className="icons">
             {/* <li><a href="#"><span class="fab fa-facebook-f"></span></a></li> */}
-            <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+            {/* <li><a href="#"><span class="fab fa-twitter"></span></a></li> */}
             <li><a href="#"><span class="fab fa-linkedin " ></span></a></li>
             {/* <li><a href="#"><span class="fab fa-instagram"></span></a></li> */}
          </div>
@@ -30,6 +63,8 @@ function PersonCard(props){
             </div> */}
         </>
     )
+  }
+
 }
 
 export default PersonCard;
