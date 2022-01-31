ff1c8b86b496cfd046a88ddba28743930360df80
mittalkeshav999 <89253638+mittalkeshav999@users.noreply.github.com>
2022-01-31 23:18:14 +0530

    Linking done

--- sae-nitkkr/src/Components/AutokritiEvent/AutokritiEvent.js
+++ sae-nitkkr/src/Components/AutokritiEvent/AutokritiEvent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./AutokritiEvent.css";
 
 function AutokritiEvent() {
@@ -7,19 +8,25 @@ function AutokritiEvent() {
       <div className="AutokritiEvent">
         <div className="AutoKritiEvent-text1">Sat, 12 - Feb Onwards</div>
         <div>
-        <div className="AutoKritiEvent-text2" style={{marginBottom: "20px"}}>
-          e-Autokriti 2.0
-        </div>
-        <div className="AutoKritiEvent-text3">
+          <div
+            className="AutoKritiEvent-text2"
+            style={{ marginBottom: "20px" }}
+          >
+            e-Autokriti 2.0
+          </div>
+          <div className="AutoKritiEvent-text3">
             {" "}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Is
-            Here!!{" "}
-        </div>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Is Here!!{" "}
+          </div>
         </div>
         <div className="AutokritiEvent-button">
           <div className="AutokritiEvent-button">
-            <button className="AutokritiEvent-button1">Register Here</button>
-            <button className="AutokritiEvent-button2">What is it ?</button>
+            <Link to="/autokriti/register" target="_blank">
+              <button className="AutokritiEvent-button1">Register Here</button>
+            </Link>
+            <Link to="/autokriti?ambassador">
+              <button className="AutokritiEvent-button2">What is it ?</button>
+            </Link>
           </div>
           {/* <div >
           
