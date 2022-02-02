8b362af9f640e975a47f7f693dae8eb5fd112f12
arnavgoel18 <arnav18.goel@gmail.com>
2022-02-02 12:19:03 +0530

    Information Change

--- sae-nitkkr/src/Components/AutokritiEvent/Context/CheckState.js
+++ sae-nitkkr/src/Components/AutokritiEvent/Context/CheckState.js
@@ -11,7 +11,7 @@ function CheckState(props) {
       show: false,
       arrownature: "down",
       description:
-        "This 5 days workshop begins with an overview of vehicle, followed by the power-flow through drivetrain,steering system&braking system.By the end of the day,you'll have deep insight of how your vehicle works",
+        "Starting with the various components composing an automobile’s transmission, Day 1 involves- 1. Relation between power, torque and rpm. 2. Why do need transmission 3. Different types of transmission 4. Gear types and related terminology. By the end of the day, you’ll have deep insight of what really happens when gears are changed or when clutch pedal is pressed.",
     },
     {
       id: "2",
@@ -19,7 +19,7 @@ function CheckState(props) {
       show: false,
       arrownature: "down",
       description:
-        "The subsequent session takes you deeper into the manufacturing part. Here you’ll get to know the designing fundamentals of gearbox, axle, steering and braking system.",
+        "Once we are thorough with the functioning of different units of a drivetrain, we'll move to the designing part. And the first step towards designing is the knowledge of what numbers we wish achieve from our machine i.e., 1. Max Torque, power and rpm 2. No. of gears, etc. . The subsequent session takes you deeper into designing fundamentals of gearbox, axle and shafts. And together we’ll do the calculations, taking into consideration all the constraints (space, cost and design limitations), to get the desired output.",
     },
     {
       id: "3",
@@ -27,7 +27,7 @@ function CheckState(props) {
       show: false,
       arrownature: "down",
       description:
-        "Once we have all the data for the components, the next step is modelling of the parts. So, the next we have is a session entirely dedicated to CAD in Solidworks. We’d go from basics of Solidworks to 3D modelling and assembly of the components from previous session. ",
+        "With all the required data in hand, the next step is modelling of the parts. So, the succeeding session is entirely dedicated to CAD in SolidWorks. We’d go from basics of SolidWorks to 3D modelling and assembly of the components from previous session.",
     },
     {
       id: "4",
@@ -35,7 +35,7 @@ function CheckState(props) {
       show: false,
       arrownature: "down",
       description:
-        "Ensuing it will be a session on Ansys, encompassing the structural analysis of designed components. An another software added to your skillset! ",
+        "Ensuing it will be a session on Ansys, encompassing the structural analysis of designed components. From meshing to material selection and selecting boundary conditions, the session will be hugely insightful. An another software added to your skillset!",
     },
 
     {
@@ -44,7 +44,7 @@ function CheckState(props) {
       show: false,
       arrownature: "down",
       description:
-        "And finally, you’ll get a hands on experience through a project, that will ensure the use of all your knowledge up till now. Wouldn’t it be exciting? ",
+        "And finally, you’ll get a hands on experience through a project involving implementing the learning from all 4 days. Wouldn’t it be exciting? When all is said and done, you’ll be in your own right to judge how a mechanism can be designed.",
     },
   ];
 
