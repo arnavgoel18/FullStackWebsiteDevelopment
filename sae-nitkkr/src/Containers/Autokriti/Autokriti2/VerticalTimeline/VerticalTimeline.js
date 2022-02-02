70ce3f094621b38d3723320f6987c130c1b7dcb5
arnavgoel18 <arnav18.goel@gmail.com>
2022-02-02 14:38:00 +0530

    Image Placement

--- sae-nitkkr/src/Containers/Autokriti/Autokriti2/VerticalTimeline/VerticalTimeline.js
+++ sae-nitkkr/src/Containers/Autokriti/Autokriti2/VerticalTimeline/VerticalTimeline.js
@@ -8,7 +8,7 @@ function VerticalTimeline(){
                 <TimelineItem
                     key="001"
                     dateText="12 / 02 / 2022"
-                    dateInnerStyle={{ background: '#e86971', color: '#000' }}
+                    dateInnerStyle={{ background: '#76bb7f', color: '#fff' }}
                     bodyContainerStyle={{
                     background: '#ddd',
                     padding: '20px',
@@ -16,16 +16,16 @@ function VerticalTimeline(){
                     boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
                     }}
                 >
-                    <h3>Day 1</h3>
-                    <h4>Introduction</h4>
+                    <h3 style={{ color: '#61b8ff', marginBottom: '15px' }}>Day 1</h3>
+                    <h4 style={{ color: '#61b8ff', marginBottom: '15px' }}>Introduction</h4>
                     <p>
-                    Starting with the various components composing an automobile’s transmission, Day 1 involves- 1. Relation between power, torque and rpm. 2. Why do need transmission 3. Different types of transmission 4. Gear types and related terminology. By the end of the day, you’ll have deep insight of what really happens when gears are changed or when clutch pedal is pressed.
+                        Starting with the various components composing an automobile’s transmission, Day 1 involves- 1. Relation between power, torque and rpm. 2. Why do need transmission 3. Different types of transmission 4. Gear types and related terminology. By the end of the day, you’ll have deep insight of what really happens when gears are changed or when clutch pedal is pressed.
                     </p>
                 </TimelineItem>
                 <TimelineItem
                     key="002"
-                    dateText="04/2009 – 11/2010"
-                    dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
+                    dateText="13 / 02 / 2022 "
+                    dateInnerStyle={{ background: '#ffde00', color: '#fff' }}
                     bodyContainerStyle={{
                     background: '#ddd',
                     padding: '20px',
@@ -33,82 +33,64 @@ function VerticalTimeline(){
                     boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
                     }}
                 >
-                    <h3 style={{ color: '#61b8ff' }}>Title, Company</h3>
-                    <h4 style={{ color: '#61b8ff' }}>Subtitle</h4>
+                    <h3 style={{ color: '#61b8ff', marginBottom: '15px' }}>Day 2</h3>
+                    <h4 style={{ color: '#61b8ff', marginBottom: '15px' }}>Fundamentals</h4>
                     <p>
-                    Once we are thorough with the functioning of different units of a drivetrain, we'll move to the designing part. And the first step towards designing is the knowledge of what numbers we wish achieve from our machine i.e., 1. Max Torque, power and rpm 2. No. of gears, etc. . The subsequent session takes you deeper into designing fundamentals of gearbox, axle and shafts. And together we’ll do the calculations, taking into consideration all the constraints (space, cost and design limitations), to get the desired output.
-                    </p>
-                    <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
+                        Once we are thorough with the functioning of different units of a drivetrain, we'll move to the designing part. And the first step towards designing is the knowledge of what numbers we wish achieve from our machine i.e., 1. Max Torque, power and rpm 2. No. of gears, etc. . The subsequent session takes you deeper into designing fundamentals of gearbox, axle and shafts. And together we’ll do the calculations, taking into consideration all the constraints (space, cost and design limitations), to get the desired output.
                     </p>
                 </TimelineItem>
-                {/* <TimelineItem
+                <TimelineItem
                     key="003"
-                    dateComponent={(
-                    <div
-                        style={{
-                        display: 'block',
-                        float: 'left',
-                        padding: '10px',
-                        background: 'rgb(150, 150, 150)',
-                        color: '#fff',
-                        }}
-                    >
-                        11/2008 – 04/2009
-                    </div>
-                    )}
+                    dateText="TBC"
+                    dateInnerStyle={{ background: '#ffbc00', color: '#fff' }}
+                    bodyContainerStyle={{
+                    background: '#ddd',
+                    padding: '20px',
+                    borderRadius: '8px',
+                    boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+                    }}
                 >
-                    <h3>Title, Company</h3>
-                    <h4>Subtitle</h4>
+                    <h3 style={{ color: '#61b8ff', marginBottom: '15px'}}>Day 3</h3>
+                    <h4 style={{ color: '#61b8ff', marginBottom: '15px'}}>Modelling Fundamentals Part 1</h4>
                     <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
-                    </p>
-                    <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
-                    </p>
-                    <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
+                        With all the required data in hand, the next step is modelling of the parts. So, the succeeding session is entirely dedicated to CAD in SolidWorks. We’d go from basics of SolidWorks to 3D modelling and assembly of the components from previous session.
                     </p>
                 </TimelineItem>
                 <TimelineItem
                     key="004"
-                    dateText="08/2008 – 11/2008"
-                    dateInnerStyle={{ background: '#76bb7f' }}
+                    dateText="TBC"
+                    dateInnerStyle={{ background: '#fd9600', color: '#fff' }}
+                    bodyContainerStyle={{
+                    background: '#ddd',
+                    padding: '20px',
+                    borderRadius: '8px',
+                    boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+                    }}
                 >
-                    <h3>Title, Company</h3>
-                    <h4>Subtitle</h4>
+                    <h3 style={{ color: '#61b8ff', marginBottom: '15px'}}>Day 4</h3>
+                    <h4 style={{ color: '#61b8ff', marginBottom: '15px'}}>Modelling Fundamentals Part 2</h4>
                     <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
+                        Ensuing it will be a session on Ansys, encompassing the structural analysis of designed components. From meshing to material selection and selecting boundary conditions, the session will be hugely insightful. An another software added to your skillset!
                     </p>
+                </TimelineItem>
+                <TimelineItem
+                    key="005"
+                    dateText="Project"
+                    dateInnerStyle={{ background: '#e86971', color: '#fff' }}
+                    bodyContainerStyle={{
+                    background: '#ddd',
+                    padding: '20px',
+                    borderRadius: '8px',
+                    boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+                    }}
+                >
+                    <h3 style={{ color: '#61b8ff', marginBottom: '15px'}}>Day 4</h3>
+                    <h4 style={{ color: '#61b8ff', marginBottom: '15px'}}>Modelling Fundamentals Part 2</h4>
                     <p>
-                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-                    est.
+                        And finally, you’ll get a hands on experience through a project involving implementing the learning from all 4 days. Wouldn’t it be exciting? When all is said and done, you’ll be in your own right to judge how a mechanism can be designed.
                     </p>
-                </TimelineItem> */}
-                </Timeline>
+                </TimelineItem>
+            </Timeline>
         </>
     )
 }
