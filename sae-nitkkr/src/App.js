7a9557ea3f8cc43d386b7b26930b796c2d1c94fe
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2022-02-03 17:39:10 +0530

    quiz response route added

--- sae-nitkkr/src/App.js
+++ sae-nitkkr/src/App.js
@@ -30,6 +30,7 @@ import ShowAmbLogin from "./Containers/ShowAmbassador/ShowAmbLogin";
 import CheckState from './Components/AutokritiEvent/Context/CheckState';
 
 import Autokriti2 from './Containers/Autokriti/Autokriti2/Autorkriti2'
+import DisplayAllQuizes from "./Containers/Backend/DisplayAllQuizes/DisplayAllQuizes";
 
 //import RegistrationForm from "./Containers/Autokriti/RegistrationForm/RegistrationForm";
 
@@ -56,9 +57,10 @@ function App() {
             <Route path="/admin/ambassadorInfo" exact component={() => <DisplayAmb/>}/>
             <Route path="/admin/contactUsInfo" exact component={() => <DisplayContact/>}/>
             <Route path="/admin/finalAmbassador" exact component={() => <DisplayFinalAmbassador/>}/>
-            <Route path="admin/signUpSheet" exact component={() => <DisplaySignUpSheet/>}/>
-            <Route path="admin/quizResponses" exact component={() => <DisplayQuizResponses/>}/>
+            <Route path="/admin/signUpSheet" exact component={() => <DisplaySignUpSheet/>}/>
+            <Route path="/admin/quizResponses" exact component={() => <DisplayQuizResponses/>}/>
 
+<Route path="/quizResponses" exact component={() => <DisplayAllQuizes/>}/>
             <Route path="/admin/displayBlogs" exact component={() => <DisplayBlogs/>}/>
             <Route path="/admin/displayBlogs/editor" exact component={() => <QuillEditor/>}/>
             {/* <Route path="/registrationForm" exact component={()=><RegistrationForm/>}/> */}
