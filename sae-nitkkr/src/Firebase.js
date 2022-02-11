11271d5f5c2a2e2f03bec92d639c51eecbece904
kritika <kritikaagrawal75@gmail.com>
2022-02-11 20:13:48 +0530

    urgent

--- sae-nitkkr/src/Firebase.js
+++ sae-nitkkr/src/Firebase.js
@@ -3,21 +3,21 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   //SAE (DO NOT USE THIS FOR TESTING - ONLY DEPLOYEMENT)
-  apiKey: "AIzaSyAw99i_BGoXy4tM-tyM-zpFRXH6uZhHD0U",
-  authDomain: "autokriti-ambassadors-2022.firebaseapp.com",
-  projectId: "autokriti-ambassadors-2022",
-  storageBucket: "autokriti-ambassadors-2022.appspot.com",
-  messagingSenderId: "1064644738570",
-  appId: "1:1064644738570:web:fc0e1fe0cdbd62e5c829f3"
+  // apiKey: "AIzaSyAw99i_BGoXy4tM-tyM-zpFRXH6uZhHD0U",
+  // authDomain: "autokriti-ambassadors-2022.firebaseapp.com",
+  // projectId: "autokriti-ambassadors-2022",
+  // storageBucket: "autokriti-ambassadors-2022.appspot.com",
+  // messagingSenderId: "1064644738570",
+  // appId: "1:1064644738570:web:fc0e1fe0cdbd62e5c829f3"
 
   //Kritika
-    // apiKey: "AIzaSyAfJTGsx3HsGeJlkIeSTf_VsrlgEw2pfrY",
-    // authDomain: "sae-website-127b1.firebaseapp.com",
-    // projectId: "sae-website-127b1",
-    // storageBucket: "sae-website-127b1.appspot.com",
-    // messagingSenderId: "636129718123",
-    // appId: "1:636129718123:web:555343dd6f0255ecbe262e",
-    // measurementId: "G-BHECE0F8CX"
+     apiKey: "AIzaSyAfJTGsx3HsGeJlkIeSTf_VsrlgEw2pfrY",
+     authDomain: "sae-website-127b1.firebaseapp.com",
+     projectId: "sae-website-127b1",
+     storageBucket: "sae-website-127b1.appspot.com",
+     messagingSenderId: "636129718123",
+     appId: "1:636129718123:web:555343dd6f0255ecbe262e",
+     measurementId: "G-BHECE0F8CX"
 
   // SAE TESTER
   //  apiKey: "AIzaSyAfGWoUlOwoP5PHuRy4FP9Ic1w1YxY2uFA",
