f3c9cb17e84e3b3a83adcce99b548054d7b39e8d
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2022-06-24 23:09:47 +0530

    version 18 updated

--- sae-nitkkr/src/index.js
+++ sae-nitkkr/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-  ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+const root=ReactDOM.createRoot( document.getElementById('root'));
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
