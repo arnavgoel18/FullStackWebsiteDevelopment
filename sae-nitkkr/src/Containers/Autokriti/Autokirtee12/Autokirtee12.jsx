772d648213e3a62e7773339406278085528b32c4
Himanshu <hkhatri2107@gmail.com>
2022-07-25 18:12:39 +0530

    autokirtee12

---
+++ sae-nitkkr/src/Containers/Autokriti/Autokirtee12/Autokirtee12.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Autokirtee12.css";
+function Autokirtee12() {
+  return (
+    <>
+      <div className="Autokirtee12_popup_main">
+        <div className="Autokirtee12_outer">
+          <div className="Autokirtee12_title">
+            <p>AUTOKRITI 12.0</p>
+          </div>
+          <div className="Autokirtee12_text">
+            <p>
+              he main body of a book or other piece of writing, as distinct from
+              other material such as notes, appendices, and illustrations: "the
+              pictures are clear and relate well to the text" synonyms: words ·
+              wording · subject matter · content · contents · body · main body ·
+            </p>
+          </div>
+          <div className="Autokirtee12_rest">
+         
+            <div className="Autokirtee12_left">
+              <div className="Autokirtee12_content">
+                <div className="Autokirtee12_content_left">
+                  <div className="Autokirtee12_content_list">
+                    <p> Transmission</p>
+                  </div>
+                  <div className="Autokirtee12_content_list">
+                    <p> Vehical Dynamics</p>
+                  </div>
+                  <div className="Autokirtee12_content_list">
+                    <p> Brakes</p>
+                  </div>
+                </div>
+                <div className="Autokirtee12_content_right">
+                  <div className="Autokirtee12_content_list">
+                  <img src="https://img.icons8.com/ios/50/000000/engine.png"/>
+                    <p>
+                    
+                    Overhauling of engines </p>
+                  </div>
+                  <div className="Autokirtee12_content_list">
+                    <p> Drive train</p>
+                  </div>
+                  <div className="Autokirtee12_content_list">
+                    <p>DAQ</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="Autokirtee12_buttons">
+                <button type="button" className="Autokirtee_12_button">
+                  MORE INFORMATION
+                </button>
+                <button type="button" className="Autokirtee_12_button">
+                  REGISTER NOW
+                </button>
+              </div>
+            </div>
+
+            <div className="Autokirtee12_img_container">
+              <img
+                src="https://www.picamon.com/wp-content/uploads/2020/10/Picamon-northern-lights-0-5f8b42955e1ad"
+                alt=""
+                className="Autokirtee12_img"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Autokirtee12;
