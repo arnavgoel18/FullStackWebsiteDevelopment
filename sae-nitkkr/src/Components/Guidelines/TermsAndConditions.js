872f9616191fae7cf1f79ffb0342b188811233a3
Himanshu <hkhatri2107@gmail.com>
2022-08-02 00:21:16 +0530

    formatting of policies

--- sae-nitkkr/src/Components/Guidelines/TermsAndConditions.js
+++ sae-nitkkr/src/Components/Guidelines/TermsAndConditions.js
@@ -4,7 +4,7 @@ function TermsAndConditions(){
 return(
     <div className="tacbody">
     <h2><strong>Terms and Conditions</strong></h2>
-
+    <br/>
 <p>Welcome to SAE NIT Kurukshetra!</p>
 
 <p>These terms and conditions outline the rules and regulations for the use of SAE NIT Kurukshetra's Website, located at https://saenitkurukshetra.in.</p>
@@ -12,15 +12,15 @@ return(
 <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use SAE NIT Kurukshetra if you do not agree to take all of the terms and conditions stated on this page.</p>
 
 <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
-
+<br/><br/>
 <h3><strong>Cookies</strong></h3>
-
+<br/>
 <p>We employ the use of cookies. By accessing SAE NIT Kurukshetra, you agreed to use cookies in agreement with the SAE NIT Kurukshetra's Privacy Policy. </p>
 
 <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
-
+<br/><br/>
 <h3><strong>License</strong></h3>
-
+<br/>
 <p>Unless otherwise stated, SAE NIT Kurukshetra and/or its licensors own the intellectual property rights for all material on SAE NIT Kurukshetra. All intellectual property rights are reserved. You may access this from SAE NIT Kurukshetra for your own personal use subjected to restrictions set in these terms and conditions.</p>
 
 <p>You must not:</p>
@@ -47,9 +47,9 @@ return(
 </ul>
 
 <p>You hereby grant SAE NIT Kurukshetra a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
-
+<br/><br/>
 <h3><strong>Hyperlinking to our Content</strong></h3>
-
+<br/>
 <p>The following organizations may link to our Website without prior written approval:</p>
 
 <ul>
@@ -89,31 +89,31 @@ return(
 </ul>
 
 <p>No use of SAE NIT Kurukshetra's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
-
+<br/><br/>
 <h3><strong>iFrames</strong></h3>
-
+<br/>
 <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
-
+<br/><br/>
 <h3><strong>Content Liability</strong></h3>
-
+<br/>
 <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
-
+<br/><br/>
 <h3><strong>Your Privacy</strong></h3>
-
+<br/>
 <p>Please read Privacy Policy</p>
-
+<br/><br/>
 <h3><strong>Reservation of Rights</strong></h3>
-
+<br/>
 <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
-
+<br/><br/>
 <h3><strong>Removal of links from our website</strong></h3>
-
+<br/>
 <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
 
 <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
-
+<br/><br/>
 <h3><strong>Disclaimer</strong></h3>
-
+<br/>
 <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
 
 <ul>
