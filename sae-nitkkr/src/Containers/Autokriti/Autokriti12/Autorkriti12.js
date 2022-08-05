198bef715322a3f4bf1035a261def22e6f27f6c1
Abhishek <akumar300599@gmail.com>
2022-08-05 11:41:44 +0530

    Update Autorkriti12.js

--- sae-nitkkr/src/Containers/Autokriti/Autokriti12/Autorkriti12.js
+++ sae-nitkkr/src/Containers/Autokriti/Autokriti12/Autorkriti12.js
@@ -114,62 +114,7 @@ function Autorkriti12() {
           </a>
         </div>
       </div>
-
-      <div className="register_line-container">
-        <div className="register_line-icon">
-          <GrCircleInformation />
-          <h3>Guidelines and Pricing</h3>
-        </div>
-        <p>
-          1. There are 4 Departments - <br />
-          <br />
-          <ul style={{ marginLeft: "2em" }}>
-            <li>Mechanical</li>
-            <li>EV</li>
-            <li>IOT</li>
-            <li>Softwares</li>
-          </ul>
-          <br />
-          You can choose Max 2 Departments
-        </p>
-        <p>2. Each Department/Workshop is 3 Days long</p>
-        <p>
-          3. If you choose 1 workshop, your totoal duration will be 4 Days. 3
-          Days Workshop + 1 Day Guest Lecture
-        </p>
-        <p>
-          4. If you choose 2 workshops, your total duration will be 7 Days. 3
-          Days Workshop (Department 1) + 1 Day Guest Lecture + 3 Days Workshop
-          (Department 2)
-        </p>
-        <p>
-          5. Each workshop is <b>2500/- INR</b>
-        </p>
-        <p>
-          6. Accomodation and Food is <b>500/-</b> Extra per Slot
-        </p>
-        <p>
-          7. For any queries on type of accomodation or food service, please
-          contact us{" "}
-          <Link to="/contactus" style={{ color: "blue" }}>
-            {" "}
-            here
-          </Link>
-        </p>
-        <h4>
-          {" "}
-          <p className="register_line-last">
-            Interested in automobiles, but don’t know where to start? Let’s this
-            be your first step, to the world of electrification. Join Autokriti
-            12 and be the part of this remaking
-          </p>
-        </h4>
-        <Link to="/autokriti/register">
-          <center>
-            <button className="register_line-btn">Register Here</button>
-          </center>
-        </Link>
-      </div>
+      <RegisterLine />
       <VerticalTimeline />
       <RegisterLine />
       <Footer />
