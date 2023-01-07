9a8b90a29a887291db38b9866cd2ec211b086494
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2023-01-07 18:25:59 +0530

    crowd funding form

---
+++ sae-nitkkr/src/Containers/CrowdFunding/CrowdFunding.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FundingForm from '../../Components/CrowdFunding/FundingForm'
+
+export default function CrowdFunding() {
+  return (
+    <>
+<FundingForm/>
+   
+    </>
+  )
+}
