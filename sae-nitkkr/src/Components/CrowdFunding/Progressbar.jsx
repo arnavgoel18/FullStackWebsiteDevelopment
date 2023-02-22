735641b567a8986db3f38644a654be9b355f64bb
arnavgoel18 <arnav18.goel@gmail.com>
2023-02-22 10:49:50 +0530

    .

--- sae-nitkkr/src/Components/CrowdFunding/Progressbar.jsx
+++ sae-nitkkr/src/Components/CrowdFunding/Progressbar.jsx
@@ -43,7 +43,7 @@ const ProgressBar = (props) => {
       <div className='slider' style={fillerStyles}></div>
       <div className='text'>{`${parseInt(completed)}%`} completed</div>
       <div className='text'> <span style={{color:"red"}}>Remaining time: </span> {days} days, {hours} hours {/*, {minutes} minutes, {seconds} seconds*/}</div>
-      <div className='text' > <span style={{color:"red"}}>Amount To Raise:</span> &#8377; {800000-amountReceived} </div>
+      {/* <div className='text' > <span style={{color:"red"}}>Amount To Raise:</span> &#8377; {800000-amountReceived} </div> */}
 
 
     </div>
