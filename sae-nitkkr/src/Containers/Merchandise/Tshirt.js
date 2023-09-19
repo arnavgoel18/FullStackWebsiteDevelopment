2ccf960c17e43a002819074b91e54b09b219e446
Mohit Gupta <135738292+mtg718@users.noreply.github.com>
2023-09-19 14:31:05 +0530

    Update Tshirt.js

--- sae-nitkkr/src/Containers/Merchandise/Tshirt.js
+++ sae-nitkkr/src/Containers/Merchandise/Tshirt.js
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Tshirt.css";
 import tshirt from "./tshirt.png";
  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faIndianRupee } from "@fortawesome/free-solid-svg-icons";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
-
+import NavBar from "../../Components/NavBar/NavBar";
 
 const Tshirt = () => {
   const [count, setCount] = useState(0);
+  
+  const [selectedSize, setSelectedSize]= useState(null);
+
+  const [price]= useState(450);
 
 
+  const handleClick =(size)=>{
+    setSelectedSize(size);
+  };
+
+  const handleAddToCart=()=>{
+    if(selectedSize){
+      const cartItem={
+     
+        size:selectedSize,
+        price:price,
+        quantity:count,
+      };
+
+      localStorage.setItem('cartItem',JSON.stringify(cartItem));
+    }
+  }
+
+  useEffect(()=>{
+localStorage.setItem('selectedSize',selectedSize);
+  },[selectedSize]);
 
   return (
     <div className="main-page">
+      <NavBar />
       <div className="main-block">
         <div className="image">
           <img src={tshirt} alt="" />
@@ -28,13 +53,13 @@ const Tshirt = () => {
 
           <div className="price">
             {/* <FontAwesomeIcon icon={faIndianRupee} /> */}
-            <span>Price:  450/-</span>
+            <span style={{ fontSize: "20px" }}>Price: 450/-</span>
           </div>
 
           <div className="size_chart">
             <h3 style={{ color: "red" }}>Size-Chart</h3>
 
-            <div className="size-content" style={{paddingLeft:"0px"}}>
+            <div className="size-content" style={{ paddingLeft: "0px" }}>
               <li>Material/Fabric : 100% Cotton</li>
               <li>Type:Round Neck t-shirt </li>
               <li>Half-sleeve</li>
@@ -45,11 +70,53 @@ const Tshirt = () => {
             <div className="size">
               <p style={{ fontSize: "18px" }}>Size :</p>
 
-              <div className="t-size" >S</div>
-              <div className="t-size">M</div>
-              <div className="t-size">L</div>
-              <div className="t-size">XL</div>
-              <div className="t-size">XXL</div>
+              <div
+                className="t-size"
+                onClick={() => handleClick("Small")}
+                style={{
+                  backgroundColor: selectedSize === "Small" ? "wheat" : "white",
+                }}
+              >
+                S
+              </div>
+
+              <div
+                className="t-size"
+                onClick={() => handleClick("Medium")}
+                style={{
+                  backgroundColor:
+                    selectedSize === "Medium" ? "wheat" : "white",
+                }}
+              >
+                M
+              </div>
+              <div
+                className="t-size"
+                onClick={() => handleClick("Large")}
+                style={{
+                  backgroundColor: selectedSize === "Large" ? "wheat" : "white",
+                }}
+              >
+                L
+              </div>
+              <div
+                className="t-size"
+                onClick={() => handleClick("XL")}
+                style={{
+                  backgroundColor: selectedSize === "XL" ? "wheat" : "white",
+                }}
+              >
+                XL
+              </div>
+              <div
+                className="t-size"
+                onClick={() => handleClick("XXL")}
+                style={{
+                  backgroundColor: selectedSize === "XXL" ? "wheat" : "white",
+                }}
+              >
+                XXL
+              </div>
             </div>
             <div className="quantity">
               <p style={{ fontSize: "18px" }}>Quantity :</p>
@@ -72,10 +139,7 @@ const Tshirt = () => {
             </div>
 
             <div className="cart-btn">
-              <button
-                
-                className="btn"
-              >
+              <button className="btn" onClick={handleAddToCart}>
                 Add to Cart
               </button>
             </div>
